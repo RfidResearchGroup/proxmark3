1de3416e5a5ba6c60e0257ce667bb08fd8ebbcde
adam@algroup.co.uk <adam@algroup.co.uk@ef4ab9da-24cd-11de-8aaa-f3a34680c41f>
2009-07-06 10:18:53 +0000

    argh. dropped locomread this time. going to the back of the class...

--- winsrc/command.cpp
+++ winsrc/command.cpp
@@ -2465,6 +2465,7 @@ static struct {
 	"lcd",				CmdLcd,0,			"<HEX command> <count> -- Send command/data to LCD",
 	"lcdreset",			CmdLcdReset,0,		"    Hardware reset LCD",
 	"load",				CmdLoad,1,		"<filename> -- Load trace (to graph window",
+	"locomread",			CmdLoCommandRead,0,		"<off period> <'0' period> <'1' period> <command> ['h'] -- Modulate LF reader field to send command before read (all periods in microseconds) (option 'h' for 134)",
 	"loread",			CmdLoread,0,		"['h'] -- Read 125/134 kHz LF ID-only tag (option 'h' for 134)",
 	"losamples",		CmdLosamples,0,		"[128 - 16000] -- Get raw samples for LF tag",
 	"losim",			CmdLosim,0,		"    Simulate LF tag",
