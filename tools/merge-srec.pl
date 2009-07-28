49209d37da501059b41f5563d6d27e291dea4ffa
d18c7db <d18c7db@ef4ab9da-24cd-11de-8aaa-f3a34680c41f>
2009-07-28 01:03:09 +0000

    merge-srec.pl create proper S19 file with checksums.

--- tools/merge-srec.pl
+++ tools/merge-srec.pl
@@ -6,13 +6,18 @@ open(FLASH, $flashFile) or die "$flashFile: $!\n";
 
 while(<FLASH>) {
 	print if /^S3/;
+	$EOF_record = $_ if /^S[789]/;
 }
 
 open(RAM, $ramFile) or die "$ramFile: $!\n";
 
 while(<RAM>) {
-	if(/^S3(..)(........)(.*)/) {
+	if(/^S3(..)(........)(.*)([0-9a-fA-F]{2})/) {
 		$addr = sprintf('%08X', hex($2) - 0x00200000 + 0x200);
-		print "S3$1$addr$3\n";
+		$line = "$1$addr$3";
+		$checksum = 0;
+		$checksum += $_ foreach map(hex, unpack("a2"x40, $line));
+		print "S3$line", sprintf("%02X", ($checksum%256)^0xff ), "\n";
 	}
 }
+print $EOF_record;
