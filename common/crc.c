68d9d60a4d5a4696faf3e36a9a8a7f10ed5bdd22
henryk@ploetzli.ch <henryk@ploetzli.ch@ef4ab9da-24cd-11de-8aaa-f3a34680c41f>
2009-11-25 20:41:41 +0000

    Add generic CRC calculation code

---
+++ common/crc.c
@@ -0,0 +1,41 @@
+/*
+ * crc.c
+ *
+ * Generic CRC calculation code.
+ * 
+ */
+
+#include "crc.h"
+
+void crc_init(crc_t *crc, int order, uint32_t polynom, uint32_t initial_value, uint32_t final_xor)
+{
+	crc->order = order;
+	crc->polynom = polynom;
+	crc->initial_value = initial_value;
+	crc->final_xor = final_xor;
+	crc->mask = (1L<<order)-1;
+	crc_clear(crc);
+}
+
+void crc_update(crc_t *crc, uint32_t data, int data_width)
+{
+	int i;
+	for(i=0; i<data_width; i++) {
+		int oldstate = crc->state;
+		crc->state = crc->state >> 1;
+		if( (oldstate^data) & 1 ) {
+			crc->state ^= crc->polynom;
+		}
+		data >>= 1;
+	}
+}
+
+void crc_clear(crc_t *crc)
+{
+	crc->state = crc->initial_value & crc->mask;
+}
+
+uint32_t crc_finish(crc_t *crc)
+{
+	return ( crc->state ^ crc->final_xor ) & crc->mask;
+}
