85f2819c8a96a1f28d90537975165a02fd019642
marcansoft <marcansoft@ef4ab9da-24cd-11de-8aaa-f3a34680c41f>
2010-02-26 14:02:08 +0000

    Add extra elf.h constants

--- client/elf.h
+++ client/elf.h
@@ -39,12 +39,33 @@ typedef struct {
 	uint16_t e_shtrndx;
 } __attribute__((__packed__)) Elf32_Ehdr;
 
-#define PT_NULL     0
-#define PT_LOAD     1
-#define PT_DYNAMIC  2
-#define PT_INTERP   3
-#define PT_NOTE     4
-#define PT_SHLIB    5
-#define PT_PHDR     6
+#define PT_NULL      0
+#define PT_LOAD      1
+#define PT_DYNAMIC   2
+#define PT_INTERP    3
+#define PT_NOTE      4
+#define PT_SHLIB     5
+#define PT_PHDR      6
+
+#define ELFCLASS32   1
+#define ELFCLASS64   2
+
+#define ELFDATA2LSB  1
+#define ELFDATA2MSB  2
+
+#define EV_CURRENT   1
+
+#define ET_NONE      0
+#define ET_REL       1
+#define ET_EXEC      2
+#define ET_DYN       3
+#define ET_CORE      4
+
+#define EM_ARM       0x28
+
+#define PF_R         4
+#define PF_W         2
+#define PF_X         1
+
 #endif
 
