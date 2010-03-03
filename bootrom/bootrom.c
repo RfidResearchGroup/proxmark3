24b182d01b2ede6d4689db9d947d21fdf8e5dfd9
marcansoft <marcansoft@ef4ab9da-24cd-11de-8aaa-f3a34680c41f>
2010-03-03 13:33:24 +0000

    Remove 512k support, fix flash waitstates
    512k chip support isn't complete anyway and the incorrect clock would've
    ended up destroying Flash anyway. The 256k waitstates were set to 0,
    which is outside the spec. Set them to 1, which is allowed.

--- bootrom/bootrom.c
+++ bootrom/bootrom.c
@@ -256,19 +256,9 @@ void BootROM(void)
     LED_B_OFF();
     LED_A_OFF();
 
-    // if 512K FLASH part - TODO make some defines :)
-    if ((AT91C_BASE_DBGU->DBGU_CIDR & 0xf00) == 0xa00) {
-	    AT91C_BASE_EFC0->EFC_FMR =
-	    	MC_FLASH_MODE_FLASH_WAIT_STATES(1) |
-			MC_FLASH_MODE_MASTER_CLK_IN_MHZ(0x48);
-	    AT91C_BASE_EFC1->EFC_FMR =
-	    	MC_FLASH_MODE_FLASH_WAIT_STATES(1) |
-			MC_FLASH_MODE_MASTER_CLK_IN_MHZ(0x48);
-    } else {
-	    AT91C_BASE_EFC0->EFC_FMR =
-	    	MC_FLASH_MODE_FLASH_WAIT_STATES(0) |
-			MC_FLASH_MODE_MASTER_CLK_IN_MHZ(48);
-    }
+	AT91C_BASE_EFC0->EFC_FMR =
+		MC_FLASH_MODE_FLASH_WAIT_STATES(1) |
+		MC_FLASH_MODE_MASTER_CLK_IN_MHZ(48);
 
     // Initialize all system clocks
     ConfigClocks();
