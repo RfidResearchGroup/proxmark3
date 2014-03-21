94faa59465751fd9dc90c8bdf430450eb2cfea17
PenturaLabs <andy.davies@pentura.com>
2014-03-21 07:51:51 +0000

    Update cmdlfio.h

--- client/cmdlfio.h
+++ client/cmdlfio.h
@@ -1,11 +1,4 @@
-//-----------------------------------------------------------------------------
-// Copyright (C) 2010 iZsh <izsh at fail0verflow.com>
-//
-// This code is licensed to you under the terms of the GNU GPL, version 2 or,
-// at your option, any later version. See the LICENSE.txt file for the text of
-// the license.
-//-----------------------------------------------------------------------------
-// Low frequency EM4x commands
+// Low frequency Kantech commands
 //-----------------------------------------------------------------------------
 
 #ifndef CMDLFIO_H__
