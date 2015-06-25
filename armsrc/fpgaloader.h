5ce7e22af68d8fc45cd08baff67d6e49ac584f43
iceman1001 <iceman@iuse.se>
2015-06-25 12:29:41 +0200

    ADD:  forgot the zlib and some new fpga compress file.s

---
+++ armsrc/fpgaloader.h
@@ -0,0 +1,71 @@
+//-----------------------------------------------------------------------------
+// Jonathan Westhues, April 2006
+// iZsh <izsh at fail0verflow.com>, 2014
+//
+// This code is licensed to you under the terms of the GNU GPL, version 2 or,
+// at your option, any later version. See the LICENSE.txt file for the text of
+// the license.
+//-----------------------------------------------------------------------------
+// Routines to load the FPGA image, and then to configure the FPGA's major
+// mode once it is configured.
+//-----------------------------------------------------------------------------
+
+void FpgaSendCommand(uint16_t cmd, uint16_t v);
+void FpgaWriteConfWord(uint8_t v);
+void FpgaDownloadAndGo(int bitstream_version);
+void FpgaGatherVersion(int bitstream_version, char *dst, int len);
+void FpgaSetupSsc(void);
+void SetupSpi(int mode);
+bool FpgaSetupSscDma(uint8_t *buf, int len);
+#define FpgaDisableSscDma(void)	AT91C_BASE_PDC_SSC->PDC_PTCR = AT91C_PDC_RXTDIS;
+#define FpgaEnableSscDma(void) AT91C_BASE_PDC_SSC->PDC_PTCR = AT91C_PDC_RXTEN;
+void SetAdcMuxFor(uint32_t whichGpio);
+
+// definitions for multiple FPGA config files support
+#define FPGA_BITSTREAM_MAX 2	// the total number of FPGA bitstreams (configs)
+#define FPGA_BITSTREAM_ERR 0
+#define FPGA_BITSTREAM_LF 1
+#define FPGA_BITSTREAM_HF 2
+
+
+// Definitions for the FPGA commands.
+#define FPGA_CMD_SET_CONFREG						(1<<12)
+#define FPGA_CMD_SET_DIVISOR						(2<<12)
+#define FPGA_CMD_SET_USER_BYTE1						(3<<12)
+// Definitions for the FPGA configuration word.
+// LF
+#define FPGA_MAJOR_MODE_LF_ADC						(0<<5)
+#define FPGA_MAJOR_MODE_LF_EDGE_DETECT				(1<<5)
+#define FPGA_MAJOR_MODE_LF_PASSTHRU					(2<<5)
+// HF
+#define FPGA_MAJOR_MODE_HF_READER_TX				(0<<5)
+#define FPGA_MAJOR_MODE_HF_READER_RX_XCORR			(1<<5)
+#define FPGA_MAJOR_MODE_HF_SIMULATOR				(2<<5)
+#define FPGA_MAJOR_MODE_HF_ISO14443A				(3<<5)
+// BOTH
+#define FPGA_MAJOR_MODE_OFF							(7<<5)
+// Options for LF_ADC
+#define FPGA_LF_ADC_READER_FIELD					(1<<0)
+// Options for LF_EDGE_DETECT
+#define FPGA_CMD_SET_EDGE_DETECT_THRESHOLD			FPGA_CMD_SET_USER_BYTE1
+#define FPGA_LF_EDGE_DETECT_READER_FIELD 			(1<<0)
+#define FPGA_LF_EDGE_DETECT_TOGGLE_MODE				(1<<1)
+// Options for the HF reader, tx to tag
+#define FPGA_HF_READER_TX_SHALLOW_MOD				(1<<0)
+// Options for the HF reader, correlating against rx from tag
+#define FPGA_HF_READER_RX_XCORR_848_KHZ				(1<<0)
+#define FPGA_HF_READER_RX_XCORR_SNOOP				(1<<1)
+#define FPGA_HF_READER_RX_XCORR_QUARTER_FREQ		(1<<2)
+// Options for the HF simulated tag, how to modulate
+#define FPGA_HF_SIMULATOR_NO_MODULATION				(0<<0)
+#define FPGA_HF_SIMULATOR_MODULATE_BPSK				(1<<0)
+#define FPGA_HF_SIMULATOR_MODULATE_212K				(2<<0)
+#define FPGA_HF_SIMULATOR_MODULATE_424K				(4<<0)
+#define FPGA_HF_SIMULATOR_MODULATE_424K_8BIT		0x5//101
+
+// Options for ISO14443A
+#define FPGA_HF_ISO14443A_SNIFFER					(0<<0)
+#define FPGA_HF_ISO14443A_TAGSIM_LISTEN				(1<<0)
+#define FPGA_HF_ISO14443A_TAGSIM_MOD				(2<<0)
+#define FPGA_HF_ISO14443A_READER_LISTEN				(3<<0)
+#define FPGA_HF_ISO14443A_READER_MOD				(4<<0)
---
+++ client/fpga_compress.c
@@ -0,0 +1,287 @@
+//-----------------------------------------------------------------------------
+// This code is licensed to you under the terms of the GNU GPL, version 2 or,
+// at your option, any later version. See the LICENSE.txt file for the text of
+// the license.
+//-----------------------------------------------------------------------------
+// Compression tool for FPGA config files. Compress several *.bit files at
+// compile time. Decompression is done at run time (see fpgaloader.c).
+// This uses the zlib library tuned to this specific case. The small file sizes
+// allow to use "insane" parameters for optimum compression ratio.
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "zlib.h"
+
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
+// zlib configuration
+#define COMPRESS_LEVEL			9		// use best possible compression
+#define COMPRESS_WINDOW_BITS	15		// default = max = 15 for a window of 2^15 = 32KBytes
+#define COMPRESS_MEM_LEVEL		9		// determines the amount of memory allocated during compression. Default = 8.
+/* COMPRESS_STRATEGY can be 
+	Z_DEFAULT_STRATEGY (the default), 
+	Z_FILTERED (more huffmann, less string matching),
+	Z_HUFFMAN_ONLY (huffman only, no string matching)
+	Z_RLE (distances limited to one)
+	Z_FIXED (prevents the use of dynamic Huffman codes)
+*/	
+#define	COMPRESS_STRATEGY		Z_DEFAULT_STRATEGY
+// zlib tuning parameters:
+#define COMPRESS_GOOD_LENGTH		258
+#define	COMPRESS_MAX_LAZY			258	
+#define	COMPRESS_MAX_NICE_LENGTH	258
+#define	COMPRESS_MAX_CHAIN			8192
+
+#define FPGA_INTERLEAVE_SIZE	288 	// (the FPGA's internal config frame size is 288 bits. Interleaving with 288 bytes should give best compression)
+#define FPGA_CONFIG_SIZE		42336	// our current fpga_[lh]f.bit files are 42175 bytes. Rounded up to next multiple of FPGA_INTERLEAVE_SIZE
+
+static void usage(void)
+{
+	fprintf(stderr, "Usage: fpga_compress <infile1> <infile2> ... <infile_n> <outfile>\n");
+	fprintf(stderr, "          Combine n FPGA bitstream files and compress them into one.\n\n");
+	fprintf(stderr, "       fpga_compress -d <infile> <outfile>");
+	fprintf(stderr, "          Decompress <infile>. Write result to <outfile>");
+}
+
+
+static voidpf fpga_deflate_malloc(voidpf opaque, uInt items, uInt size)
+{
+	return malloc(items*size);
+}
+
+
+static void fpga_deflate_free(voidpf opaque, voidpf address)
+{
+	return free(address);
+}
+
+
+static bool all_feof(FILE *infile[], uint8_t num_infiles)
+{
+	for (uint16_t i = 0; i < num_infiles; i++) {
+		if (!feof(infile[i])) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+
+int zlib_compress(FILE *infile[], uint8_t num_infiles, FILE *outfile)
+{
+	uint8_t *fpga_config;
+	uint32_t i;
+	int ret;
+	uint8_t c;		
+	z_stream compressed_fpga_stream;
+
+	fpga_config = malloc(num_infiles * FPGA_CONFIG_SIZE);
+	
+	// read the input files. Interleave them into fpga_config[]
+	i = 0;
+	do {
+
+		if (i >= num_infiles * FPGA_CONFIG_SIZE) {
+			fprintf(stderr, "Input files too big (total > %lu bytes). These are probably not PM3 FPGA config files.\n", num_infiles*FPGA_CONFIG_SIZE);
+		for(uint16_t j = 0; j < num_infiles; j++) {
+				fclose(infile[j]);
+			}
+			return(EXIT_FAILURE);
+		}
+
+		for(uint16_t j = 0; j < num_infiles; j++) {
+			for(uint16_t k = 0; k < FPGA_INTERLEAVE_SIZE; k++) {
+				c = fgetc(infile[j]);
+				if (!feof(infile[j])) {
+					fpga_config[i++] = c;
+				} else if (num_infiles > 1) {
+					fpga_config[i++] = '\0';
+				}
+			}
+		}
+
+	} while (!all_feof(infile, num_infiles));
+
+	// initialize zlib structures
+	compressed_fpga_stream.next_in = fpga_config;
+	compressed_fpga_stream.avail_in = i;
+	compressed_fpga_stream.zalloc = fpga_deflate_malloc;
+	compressed_fpga_stream.zfree = fpga_deflate_free;
+	
+	ret = deflateInit2(&compressed_fpga_stream, 
+						COMPRESS_LEVEL,
+						Z_DEFLATED,
+						COMPRESS_WINDOW_BITS,
+						COMPRESS_MEM_LEVEL,
+						COMPRESS_STRATEGY);
+
+	// estimate the size of the compressed output
+	unsigned int outsize_max = deflateBound(&compressed_fpga_stream, compressed_fpga_stream.avail_in);
+	uint8_t *outbuf = malloc(outsize_max);
+	compressed_fpga_stream.next_out = outbuf;
+	compressed_fpga_stream.avail_out = outsize_max;
+					
+	if (ret == Z_OK) {
+		ret = deflateTune(&compressed_fpga_stream,
+							COMPRESS_GOOD_LENGTH,
+							COMPRESS_MAX_LAZY,
+							COMPRESS_MAX_NICE_LENGTH,
+							COMPRESS_MAX_CHAIN);
+	}
+	
+	if (ret == Z_OK) {
+		ret = deflate(&compressed_fpga_stream, Z_FINISH);
+	}
+	
+	fprintf(stderr, "compressed %lu input bytes to %lu output bytes\n", i, compressed_fpga_stream.total_out);
+
+	if (ret != Z_STREAM_END) {
+		fprintf(stderr, "Error in deflate(): %d %s\n", ret, compressed_fpga_stream.msg);
+		free(outbuf);
+		deflateEnd(&compressed_fpga_stream);
+		for(uint16_t j = 0; j < num_infiles; j++) {
+			fclose(infile[j]);
+		}
+		fclose(outfile);
+		free(infile);
+		free(fpga_config);
+		return(EXIT_FAILURE);
+		}
+		
+	for (i = 0; i < compressed_fpga_stream.total_out; i++) {
+		fputc(outbuf[i], outfile);
+	}	
+
+	free(outbuf);
+	deflateEnd(&compressed_fpga_stream);
+	for(uint16_t j = 0; j < num_infiles; j++) {
+		fclose(infile[j]);
+	}
+	fclose(outfile);
+	free(infile);
+	free(fpga_config);
+	
+	return(EXIT_SUCCESS);
+	
+}
+
+
+int zlib_decompress(FILE *infile, FILE *outfile)
+{
+	#define DECOMPRESS_BUF_SIZE 1024
+	uint8_t outbuf[DECOMPRESS_BUF_SIZE];
+	uint8_t inbuf[DECOMPRESS_BUF_SIZE];
+	int ret;
+	
+	z_stream compressed_fpga_stream;
+
+	// initialize zlib structures
+	compressed_fpga_stream.next_in = inbuf;
+	compressed_fpga_stream.avail_in = 0;
+	compressed_fpga_stream.next_out = outbuf;
+	compressed_fpga_stream.avail_out = DECOMPRESS_BUF_SIZE;
+	compressed_fpga_stream.zalloc = fpga_deflate_malloc;
+	compressed_fpga_stream.zfree = fpga_deflate_free;
+	
+	ret = inflateInit2(&compressed_fpga_stream, 0);
+	
+	do {
+		if (compressed_fpga_stream.avail_in == 0) {
+			compressed_fpga_stream.next_in = inbuf;
+			uint16_t i = 0;
+			do {
+				uint8_t c = fgetc(infile);
+				if (!feof(infile)) {
+					inbuf[i++] = c;
+					compressed_fpga_stream.avail_in++;
+				} else {
+					break;
+				}
+			} while (i < DECOMPRESS_BUF_SIZE);
+		}
+
+		ret = inflate(&compressed_fpga_stream, Z_SYNC_FLUSH);
+
+		if (ret != Z_OK && ret != Z_STREAM_END) {
+			break;
+		}
+
+		if (compressed_fpga_stream.avail_out == 0) {
+			for (uint16_t i = 0; i < DECOMPRESS_BUF_SIZE; i++) {
+				fputc(outbuf[i], outfile);
+			}
+			compressed_fpga_stream.avail_out = DECOMPRESS_BUF_SIZE;
+			compressed_fpga_stream.next_out = outbuf;
+		}
+	} while (ret == Z_OK);
+
+	if (ret == Z_STREAM_END) {  // reached end of input
+		uint16_t i = 0;
+		while (compressed_fpga_stream.avail_out < DECOMPRESS_BUF_SIZE) {
+			fputc(outbuf[i++], outfile);
+			compressed_fpga_stream.avail_out++;
+		}
+		fclose(outfile);
+		fclose(infile);
+		return(EXIT_SUCCESS);
+	} else {
+		fprintf(stderr, "Error. Inflate() returned error %d, %s", ret, compressed_fpga_stream.msg);
+		fclose(outfile);
+		fclose(infile);
+		return(EXIT_FAILURE);
+	}
+	
+}
+
+
+int main(int argc, char **argv)
+{
+	FILE **infiles;
+	FILE *outfile;
+	
+	if (argc == 1 || argc == 2) {
+		usage();
+		return(EXIT_FAILURE);
+	}
+	
+	if (!strcmp(argv[1], "-d")) {			// Decompress
+		infiles = calloc(1, sizeof(FILE*));
+		if (argc != 4) {
+			usage();
+			return(EXIT_FAILURE);
+		} 
+		infiles[0] = fopen(argv[2], "rb");
+		if (infiles[0] == NULL) {
+			fprintf(stderr, "Error. Cannot open input file %s", argv[2]);
+			return(EXIT_FAILURE);
+		}
+		outfile = fopen(argv[3], "wb");
+		if (outfile == NULL) {
+			fprintf(stderr, "Error. Cannot open output file %s", argv[3]);
+			return(EXIT_FAILURE);
+		}
+		return zlib_decompress(infiles[0], outfile);
+
+	} else {								// Compress
+
+		infiles = calloc(argc-2, sizeof(FILE*));
+		for (uint16_t i = 0; i < argc-2; i++) { 
+			infiles[i] = fopen(argv[i+1], "rb");
+			if (infiles[i] == NULL) {
+				fprintf(stderr, "Error. Cannot open input file %s", argv[i+1]);
+				return(EXIT_FAILURE);
+			}
+		}
+		outfile = fopen(argv[argc-1], "wb");
+		if (outfile == NULL) {
+			fprintf(stderr, "Error. Cannot open output file %s", argv[argc-1]);
+			return(EXIT_FAILURE);
+		}
+		return zlib_compress(infiles, argc-2, outfile);
+	}
+}
