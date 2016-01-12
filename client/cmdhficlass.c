ddb748a973bdcb84c6a0ab21e5fcd2b964e5c2e5
iceman1001 <iceman@iuse.se>
2016-01-12 22:49:29 +0100

    FIX, Coverity, Argument can't be negative,  CID #212324,  ftell(f) can be negative,  not allowed in malloc.

--- client/cmdhficlass.c
+++ client/cmdhficlass.c
@@ -1316,8 +1316,13 @@ int CmdHFiClassReadTagFile(const char *Cmd) {
 	long fsize = ftell(f);
 	fseek(f, 0, SEEK_SET);
 
-	uint8_t *dump = malloc(fsize);
+	if ( fsize < 0 ) {
+		PrintAndLog("Error, when getting filesize");
+		fclose(f);
+		return 1;
+	}
 
+	uint8_t *dump = malloc(fsize);
 
 	size_t bytes_read = fread(dump, 1, fsize, f);
 	fclose(f);
