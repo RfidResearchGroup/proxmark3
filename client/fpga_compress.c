7f975d7e1794cc35607f41dd0e718bf2f4e99314
iceman1001 <iceman@iuse.se>
2016-01-15 15:22:25 +0100

    FIX: one of these days I will start being more observant about argument order

--- client/fpga_compress.c
+++ client/fpga_compress.c
@@ -138,7 +138,7 @@ int zlib_compress(FILE *infile[], uint8_t num_infiles, FILE *outfile)
 		ret = deflate(&compressed_fpga_stream, Z_FINISH);
 	}
 	
-	fprintf(stderr, "compressed %lu input bytes to %u output bytes\n", i, compressed_fpga_stream.total_out);
+	fprintf(stderr, "compressed %u input bytes to %lu output bytes\n", i, compressed_fpga_stream.total_out);
 
 	if (ret != Z_STREAM_END) {
 		fprintf(stderr, "Error in deflate(): %d %s\n", ret, compressed_fpga_stream.msg);
