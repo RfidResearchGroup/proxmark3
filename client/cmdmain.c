719b2377bee69506572b54af1e210b62c296675c
iceman <iceman@iuse.se>
2016-02-10 13:22:48 +0100

    chg: what?

--- client/cmdmain.c
+++ client/cmdmain.c
@@ -120,6 +120,7 @@ int getCommand(UsbCommand* response)
     //Pick out the next unread command
     UsbCommand* last_unread = &cmdBuffer[cmd_tail];
     memcpy(response, last_unread, sizeof(UsbCommand));
+
     //Increment tail - this is a circular buffer, so modulo buffer size
     cmd_tail = (cmd_tail +1 ) % CMD_BUFFER_SIZE;
 
@@ -146,10 +147,10 @@ bool WaitForResponseTimeout(uint32_t cmd, UsbCommand* response, size_t ms_timeou
 	for ( size_t dm_seconds = 0; dm_seconds < ms_timeout/10; dm_seconds++ ) {
 
 		while( getCommand(response) ) {
-			if(response->cmd == cmd){
-				return true;
-			}
+			if(response->cmd == cmd)
+				return true;			
 		}
+
 		msleep(10); // XXX ugh
 		if (dm_seconds == 200) { // Two seconds elapsed
 			PrintAndLog("Waiting for a response from the proxmark...");
@@ -180,28 +181,25 @@ void UsbCommandReceived(UsbCommand *UC)
 	switch(UC->cmd) {
 		// First check if we are handling a debug message
 		case CMD_DEBUG_PRINT_STRING: {
-			char s[USB_CMD_DATA_SIZE+1] = {0x00};
-			size_t len = MIN(UC->arg[0],USB_CMD_DATA_SIZE);
-			memcpy(s,UC->d.asBytes,len);
-			PrintAndLog("#db# %s       ", s);
-			return;
+			char s[USB_CMD_DATA_SIZE+1];
+			memset(s, 0x00, sizeof(s));
+			size_t len = MIN(UC->arg[0], USB_CMD_DATA_SIZE);
+			memcpy(s, UC->d.asBytes, len);
+			PrintAndLog("#db# %s", s);
 		} break;
 
-		case CMD_DEBUG_PRINT_INTEGERS: {
-			PrintAndLog("#db# %08x, %08x, %08x       \r\n", UC->arg[0], UC->arg[1], UC->arg[2]);
-			return;
-		} break;
+		case CMD_DEBUG_PRINT_INTEGERS:
+			PrintAndLog("#db# %08x, %08x, %08x", UC->arg[0], UC->arg[1], UC->arg[2]);
+			break;
 
-		case CMD_DOWNLOADED_RAW_ADC_SAMPLES_125K: {
-			memcpy(sample_buf+(UC->arg[0]),UC->d.asBytes,UC->arg[1]);
-			return;
-		} break;
+		case CMD_DOWNLOADED_RAW_ADC_SAMPLES_125K:
+			memcpy( sample_buf + (UC->arg[0]), UC->d.asBytes, UC->arg[1]);
+			break;
 
 		default: {
 			storeCommand(UC);
 			break;
 		}
 	}
-
 }
 
