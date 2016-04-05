a81b99b9b1fbd8eae9f819a2d997a2cef0e2efea
iceman1001 <iceman@iuse.se>
2016-04-05 04:49:34 +0200

    ADD: @zhovner 's decryption of trace data to mfkey64

--- tools/mfkey/mfkey64.c
+++ tools/mfkey/mfkey64.c
@@ -22,22 +22,41 @@ int main (int argc, char *argv[]) {
 	printf("Recover key from only one complete authentication!\n\n");
 
 	if (argc < 6) {
-		printf(" syntax: %s <uid> <nt> <nr> <ar> <at>\n\n",argv[0]);
+		printf(" syntax: %s <uid> <nt> <{nr}> <{ar}> <{at}> [enc...]\n\n", argv[0]);
 		return 1;
 	}
+
+	int encc = argc - 6;
+	int enclen[encc];
+	uint8_t enc[encc][120]; 
 
 	sscanf(argv[1],"%x",&uid);
 	sscanf(argv[2],"%x",&nt);
 	sscanf(argv[3],"%x",&nr_enc);
 	sscanf(argv[4],"%x",&ar_enc);
 	sscanf(argv[5],"%x",&at_enc);
-
+	for (int i = 0; i < encc; i++) {
+		enclen[i] = strlen(argv[i + 6]) / 2;
+		for (int i2 = 0; i2 < enclen[i]; i2++) {
+			sscanf(argv[i+6] + i2*2,"%2x", (uint8_t*)&enc[i][i2]);
+		}
+	}
+	
 	printf("Recovering key for:\n");
+
 	printf("  uid: %08x\n",uid);
 	printf("   nt: %08x\n",nt);
 	printf(" {nr}: %08x\n",nr_enc);
 	printf(" {ar}: %08x\n",ar_enc);
 	printf(" {at}: %08x\n",at_enc);
+
+	for (int i = 0; i < encc; i++) {
+		printf("{enc%d}: ", i);
+		for (int i2 = 0; i2 < enclen[i]; i2++) {
+			printf("%02x", enc[i][i2]);
+		}
+		printf("\n");
+	}
 
 	// Generate lfsr succesors of the tag challenge
 	printf("\nLFSR succesors of the tag challenge:\n");
@@ -54,6 +73,25 @@ int main (int argc, char *argv[]) {
 	printf("  ks3: %08x\n",ks3);
 
 	revstate = lfsr_recovery64(ks2, ks3);
+
+	// Decrypting communication using keystream if presented
+	if (argc > 6 ) {
+		printf("\nDecrypted communication:\n");
+		uint8_t ks4; 
+		int rollb = 0;
+		for (int i = 0; i < encc; i++) {
+			printf("{dec%d}: ", i);
+			for (int i2 = 0; i2 < enclen[i]; i2++) {  
+				ks4 = crypto1_byte(revstate, 0, 0);
+				printf("%02x", ks4 ^ enc[i][i2]);
+				rollb += 1;
+			}
+			printf("\n");
+		}
+		for (int i = 0; i < rollb; i++)
+			lfsr_rollback_byte(revstate, 0, 0);
+	}
+  
 	lfsr_rollback_word(revstate, 0, 0);
 	lfsr_rollback_word(revstate, 0, 0);
 	lfsr_rollback_word(revstate, nr_enc, 1);
