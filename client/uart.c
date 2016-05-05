04da5cd92bdf5cbdcec127961e2499121003655e
iceman1001 <iceman@iuse.se>
2016-05-05 21:13:12 +0200

    FIX:  it seem the fcntl returns -1,..  which made the flasher fail.

--- client/uart.c
+++ client/uart.c
@@ -123,7 +123,7 @@ void uart_close(const serial_port sp) {
   // Does the system allows us to place a lock on this file descriptor
   int err = fcntl(spu->fd, F_SETLK, &fl);
   if ( err == -1) {
-     perror("fcntl");
+     //perror("fcntl");
   }  
   close(spu->fd);
   free(sp);
