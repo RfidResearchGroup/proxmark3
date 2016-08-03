8a8de2cb6d850bf2252a52e11f8e3b1428e4a382
iceman1001 <iceman@iuse.se>
2016-08-03 09:28:54 +0200

    FIX: 133853,  If you have 3items in a array,  don't start with index 4 or 3,   mental note to self, stop coding when you are tired.

--- client/cmdlfjablotron.c
+++ client/cmdlfjablotron.c
@@ -179,7 +179,7 @@ int CmdJablotronClone(const char *Cmd) {
 	UsbCommand resp;
 	UsbCommand c = {CMD_T55XX_WRITE_BLOCK, {0,0,0}};
 
-	for (int i=3; i>=0; i--) {
+	for (uint8_t i = 2; i >= 0; --i) {
 		c.arg[0] = blocks[i];
 		c.arg[1] = i;
 		clearCommandBuffer();
