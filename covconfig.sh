0baea563de3826cd6c00315ad48c09d2b4d5b556
iceman <iceman@iuse.se>
2016-09-26 17:11:01 +0200

    CHG: added some cleaning up earlier configurations,  so the new config settings will be pure.

--- covconfig.sh
+++ covconfig.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 ## 20160116, iceman
+## remove old
+rm /home/user/cov-analysis-linux-7.7.0.4/config/coverity_config.xml
+rm -rf /home/user/cov-analysis-linux-7.7.0.4/config/gcc-config-?
+rm -rf /home/user/cov-analysis-linux-7.7.0.4/config/g++-config-?
+
 
 ## Configure ARM ,  make sure you have the arm gcc in your $PATH variable.
 cov-configure -co arm-none-eabi-gcc -- -mthumb-interwork
