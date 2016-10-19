4460be68c36b9545a493d4e4479cd0bb14a576cd
iceman1001 <iceman@iuse.se>
2016-10-19 19:59:58 +0200

    FIX:  "lf sim"  after changes to ticks timers on device side,  there was a "starticks" call missing, which lead to not having any timers which is a problem for the WaitUS call.

--- armsrc/lfops.c
+++ armsrc/lfops.c
@@ -393,6 +393,8 @@ void SimulateTagLowFrequency(int period, int gap, int ledcontrol)
 	int i = 0;
 	uint8_t *tab = BigBuf_get_addr();
 
+	StartTicks();
+
 	FpgaWriteConfWord(FPGA_MAJOR_MODE_LF_EDGE_DETECT | FPGA_LF_EDGE_DETECT_READER_FIELD);
 
 	AT91C_BASE_PIOA->PIO_PER = GPIO_SSC_DOUT | GPIO_SSC_CLK;
