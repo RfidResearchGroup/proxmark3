63bc529b22f1b8cbb2f65c977bcfd14474a11201
iceman1001 <iceman@iuse.se>
2017-04-19 03:42:34 +0200

    CHG: remove clang warning (piwi)

--- zlib/inflate.c
+++ zlib/inflate.c
@@ -1524,7 +1524,7 @@ z_streamp strm;
     struct inflate_state FAR *state;
 
     if (strm == Z_NULL || strm->state == Z_NULL)
-        return (long)(((unsigned long)0 - 1) << 16);
+        return ~0UL << 16;
     state = (struct inflate_state FAR *)strm->state;
     return ((long)(state->back) << 16) +
         (state->mode == COPY ? state->length :
