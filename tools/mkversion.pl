05e160b1f2ba0eb3f626570b9ffbf04f05a4694b
iceman1001 <iceman@iuse.se>
2017-08-20 03:52:06 +0200

    fix:    https://github.com/Proxmark/proxmark3/issues/12    this should allow for both use-cases to get a nice date.
    A)    ppl uses Git to pull the lastest source.
    B)    ppl download the zipfile

--- tools/mkversion.pl
+++ tools/mkversion.pl
@@ -16,25 +16,31 @@ my $githistory = `git fetch --all`;
 my $gitversion = `git describe --dirty`;
 my $gitbranch = `git rev-parse --abbrev-ref HEAD`;
 my $clean = $gitversion =~ '-dirty' ? 0 : 1;
-my @compiletime = localtime();
+my $ctime = '';
 
+# if you are making your own fork,  change this line to reflect your fork-name
 my $fullgitinfo = 'iceman';
 
 if ( defined $gitbranch and defined $gitversion ) {
 	$fullgitinfo =  $fullgitinfo.'/'. $gitbranch . '/' . $gitversion;
+
+	my @compiletime = localtime();
+	$compiletime[4] += 1;
+	$compiletime[5] += 1900;
+	$ctime = sprintf("%6\$04i-%5\$02i-%4\$02i %3\$02i:%2\$02i:%1\$02i", @compiletime);
 } else {
 	$fullgitinfo =  $fullgitinfo.'/master/release-build (no_git)';
+
+	my @dl_time = localtime( (stat('../README.md'))[10] );
+	$dl_time[4] += 1;
+	$dl_time[5] += 1900;
+	$ctime = sprintf("%6\$04i-%5\$02i-%4\$02i %3\$02i:%2\$02i:%1\$02i", @dl_time);
 }
 
 $fullgitinfo =~ s/(\s)//g;
 
 # Crop so it fits within 50 characters
 $fullgitinfo =~ s/.{50}\K.*//s;
-
-$compiletime[4] += 1;
-$compiletime[5] += 1900;
-my $ctime = sprintf("%6\$04i-%5\$02i-%4\$02i %3\$02i:%2\$02i:%1\$02i", @compiletime);
-
 
 print <<EOF
 #include "proxmark3.h"
