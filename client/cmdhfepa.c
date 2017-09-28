7607da55f7b90e9521b0997b9935a6255317b54f
iceman1001 <iceman@iuse.se>
2017-09-28 22:33:55 +0200

    FIX: unify all calls..

--- client/cmdhfepa.c
+++ client/cmdhfepa.c
@@ -162,9 +162,7 @@ int CmdHFEPAPACEReplay(const char *Cmd)
 	return 1;
 }
 
-// UI-related stuff
-
-static const command_t CommandTable[] = {
+static command_t CommandTable[] = {
 	{"help",    CmdHelp,                   1, "This help"},
 	{"cnonces", CmdHFEPACollectPACENonces, 0, "<m> <n> <d> Acquire n>0 encrypted PACE nonces of size m>0 with d sec pauses"},
 	{"preplay", CmdHFEPAPACEReplay,        0, "<mse> <get> <map> <pka> <ma> Perform PACE protocol by replaying given APDUs"},
