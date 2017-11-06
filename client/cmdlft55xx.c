dd0b11c0ef9f544dadc7adb97033c3786bcf7c32
iceman1001 <iceman@iuse.se>
2017-11-06 15:43:15 +0100

    fix:  ..its not noise ... fix:  only assume Graphbuffer values 128 -> -127 ...

--- client/cmdlft55xx.c
+++ client/cmdlft55xx.c
@@ -1316,7 +1316,7 @@ bool AquireData( uint8_t page, uint8_t block, bool pwdmode, uint32_t password ){
 	}
 	setGraphBuf(got, sizeof(got));
 
-	return justNoise(got, sizeof(got));
+	return !justNoise_int(GraphBuffer, sizeof(got));
 }
 
 char * GetBitRateStr(uint32_t id, bool xmode) {
