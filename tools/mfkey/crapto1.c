4e1d6072d226c93fdb9a5450520a968b223e3771
iceman1001 <iceman@iuse.se>
2018-01-11 21:48:04 +0100

    fix:  increase possible state sizes

--- tools/mfkey/crapto1.c
+++ tools/mfkey/crapto1.c
@@ -551,7 +551,7 @@ struct Crypto1State* lfsr_common_prefix(uint32_t pfx, uint32_t rr, uint8_t ks[8]
 	odd = lfsr_prefix_ks(ks, 1);
 	even = lfsr_prefix_ks(ks, 0);
 
-	s = statelist = malloc((sizeof *statelist) << 20);
+	s = statelist = malloc((sizeof *statelist) << 24);
 	if(!s || !odd || !even) {
 		free(statelist);
 		statelist = 0;
