d3db40b7dde90ff8aec40370df1149e388f681cf
iceman1001 <iceman@iuse.se>
2018-01-25 20:12:33 +0100

    chg:  remove warning

--- client/emv/emv_pki.c
+++ client/emv/emv_pki.c
@@ -53,7 +53,7 @@ static unsigned char *emv_pki_decode_message(const struct emv_pk *enc_pk,
 	}
 
 	if (cert_tlv->len != enc_pk->mlen) {
-		printf("ERROR: Certificate length (%d) not equal key length (%zu)\n", cert_tlv->len, enc_pk->mlen);
+		printf("ERROR: Certificate length (%zu) not equal key length (%zu)\n", cert_tlv->len, enc_pk->mlen);
 		return NULL;
 	}
 	kcp = crypto_pk_open(enc_pk->pk_algo,
