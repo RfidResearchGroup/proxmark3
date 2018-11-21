7b943a4a16218a630a5f1060850df7075ced2f50
merlokk <807634+merlokk@users.noreply.github.com>
2018-11-21 19:44:31 +0200

    add check string - if string is hex

--- client/util.h
+++ client/util.h
@@ -190,6 +190,7 @@ extern void FillFileNameByUID(char *filenamePrefix, uint8_t * uid, const char *e
 // fill buffer from structure [{uint8_t data, size_t length},...]
 extern int FillBuffer(uint8_t *data, size_t maxDataLength, size_t *dataLength, ...);
 
+extern bool CheckStringIsHEXValue(const char *value);
 extern void hex_to_buffer(const uint8_t *buf, const uint8_t *hex_data, const size_t hex_len, 
 						  const size_t hex_max_len, const size_t min_str_len, const size_t spaces_between,
 						  bool uppercase);
