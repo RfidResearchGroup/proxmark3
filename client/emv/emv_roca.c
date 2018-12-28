690a8e604aee614ab6e6ee1b159af9d587f41d38
merlokk <807634+merlokk@users.noreply.github.com>
2018-12-29 00:31:10 +0200

    updated licanse according to changes https://gist.github.com/robstradling/f525d423c79690b72e650e2ad38a161d#file-roca-c-L138

--- client/emv/emv_roca.c
+++ client/emv/emv_roca.c
@@ -1,10 +1,12 @@
 /* roca.c - ROCA (CVE-2017-15361) fingerprint checker.
  * Written by Rob Stradling (based on https://github.com/crocs-muni/roca/blob/master/roca/detect.py)
- * Copyright (C) 2017 COMODO CA Limited
+ * Copyright (C) 2017-2018 Sectigo Limited
+ * modified 2018 iceman  (dropped openssl bignum,  now use mbedtls lib)
+ * modified 2018 merlok
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,6 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+//-----------------------------------------------------------------------------
+// EMV roca commands
+//-----------------------------------------------------------------------------
 
 #include "emv_roca.h"
 
