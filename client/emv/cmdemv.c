e058d87dc11ec2c3117d8848ea68b54a9c779e65
merlokk <807634+merlokk@users.noreply.github.com>
2019-01-10 19:28:43 +0200

    fix CDOL generation and add AC1 print result

--- client/emv/cmdemv.c
+++ client/emv/cmdemv.c
@@ -1182,17 +1182,17 @@ int CmdEMVExec(const char *cmd) {
 		PrintAndLogEx(NORMAL, "\n--> VSDC transaction.");
 		
 		PrintAndLogEx(NORMAL, "* * Calc CDOL1");
-		struct tlv *cdol_data_tlv = dol_process(tlvdb_get(tlvRoot, 0x8c, NULL), tlvRoot, 0x01); // 0x01 - dummy tag
-		if (!cdol_data_tlv) {
+		struct tlv *cdol1_data_tlv = dol_process(tlvdb_get(tlvRoot, 0x8c, NULL), tlvRoot, 0x01); // 0x01 - dummy tag
+		if (!cdol1_data_tlv) {
 			PrintAndLogEx(WARNING, "Error: can't create CDOL1 TLV.");
 			dreturn(6);
 		}
 		
-		PrintAndLogEx(NORMAL, "CDOL1 data[%d]: %s", cdol_data_tlv->len, sprint_hex(cdol_data_tlv->value, cdol_data_tlv->len));
+		PrintAndLogEx(NORMAL, "CDOL1 data[%d]: %s", cdol1_data_tlv->len, sprint_hex(cdol1_data_tlv->value, cdol1_data_tlv->len));
 		
 		PrintAndLogEx(NORMAL, "* * AC1");
 		// EMVAC_TC + EMVAC_CDAREQ --- to get SDAD
-		res = EMVAC(channel, true, (TrType == TT_CDA) ? EMVAC_TC + EMVAC_CDAREQ : EMVAC_TC, (uint8_t *)cdol_data_tlv->value, cdol_data_tlv->len, buf, sizeof(buf), &len, &sw, tlvRoot);
+		res = EMVAC(channel, true, (TrType == TT_CDA) ? EMVAC_TC + EMVAC_CDAREQ : EMVAC_TC, (uint8_t *)cdol1_data_tlv->value, cdol1_data_tlv->len, buf, sizeof(buf), &len, &sw, tlvRoot);
 		
 		if (res) {	
 			PrintAndLogEx(NORMAL, "AC1 error(%d): %4x. Exit...", res, sw);
@@ -1201,7 +1201,17 @@ int CmdEMVExec(const char *cmd) {
 
 		// process Format1 (0x80) anf print Format2 (0x77)
 		ProcessACResponseFormat1(tlvRoot, buf, len, decodeTLV);
-		
+
+		uint8_t CID = 0;
+		tlvdb_get_uint8(tlvRoot, 0x9f27, &CID);
+
+		// AC1 print result
+		PrintAndLog("");
+		if ((CID & EMVAC_AC_MASK) == EMVAC_AAC)		PrintAndLogEx(INFO, "AC1 result: AAC (Transaction declined)");
+		if ((CID & EMVAC_AC_MASK) == EMVAC_TC)		PrintAndLogEx(INFO, "AC1 result: TC (Transaction approved)");
+		if ((CID & EMVAC_AC_MASK) == EMVAC_ARQC)	PrintAndLogEx(INFO, "AC1 result: ARQC (Online authorisation requested)");
+		if ((CID & EMVAC_AC_MASK) == EMVAC_AC_MASK)	PrintAndLogEx(INFO, "AC1 result: RFU");
+
 		PrintAndLogEx(NORMAL, "\n* * Processing online request\n");
 
 		// authorization response code from acquirer
@@ -1210,21 +1220,26 @@ int CmdEMVExec(const char *cmd) {
 		tlvdb_change_or_add_node(tlvRoot, 0x8a, sizeof(HostResponse) - 1, (const unsigned char *)HostResponse);		
 		
 		// needs to send AC2 command (res == ARQC)
-		uint8_t CID = 0;
-		tlvdb_get_uint8(tlvRoot, 0x9f27, &CID);
 		if ((CID & 0xc0) == 0x80) {
 			PrintAndLogEx(NORMAL, "* * Calc CDOL2");
-			struct tlv *cdol_data_tlv = dol_process(tlvdb_get(tlvRoot, 0x8d, NULL), tlvRoot, 0x01); // 0x01 - dummy tag
-			if (!cdol_data_tlv) {
+			struct tlv *cdol2_data_tlv = dol_process(tlvdb_get(tlvRoot, 0x8d, NULL), tlvRoot, 0x01); // 0x01 - dummy tag
+			if (!cdol2_data_tlv) {
 				PrintAndLogEx(WARNING, "Error: can't create CDOL2 TLV.");
 				dreturn(6);
 			}
 			
-			PrintAndLogEx(NORMAL, "CDOL2 data[%d]: %s", cdol_data_tlv->len, sprint_hex(cdol_data_tlv->value, cdol_data_tlv->len));
+			PrintAndLogEx(NORMAL, "CDOL2 data[%d]: %s", cdol2_data_tlv->len, sprint_hex(cdol2_data_tlv->value, cdol2_data_tlv->len));
 			
 			PrintAndLogEx(NORMAL, "* * AC2");
 			
 			
+/*		// AC2
+		PRINT_INDENT(level);
+		if ((CID & EMVAC_AC2_MASK) == EMVAC_AAC2)	fprintf(f, "\tAC2: AAC (Transaction declined)\n");
+		if ((CID & EMVAC_AC2_MASK) == EMVAC_TC2)	fprintf(f, "\tAC2: TC (Transaction approved)\n");
+		if ((CID & EMVAC_AC2_MASK) == EMVAC_ARQC2)	fprintf(f, "\tAC2: not requested (ARQC)\n");
+		if ((CID & EMVAC_AC2_MASK) == EMVAC_AC2_MASK)	fprintf(f, "\tAC2: RFU\n");
+*/
 		}
 		
 	}
