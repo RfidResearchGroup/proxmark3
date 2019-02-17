535d4e6ee9e2ab49b130acafe1440aaaab4d1048
iceman1001 <iceman@iuse.se>
2019-02-17 15:37:20 +0100

    FIX: usb slow for posix systems -  https://github.com/Proxmark/proxmark3/pull/787  (@fl0-0)

--- uart/uart_posix.c
+++ uart/uart_posix.c
@@ -70,338 +70,341 @@ typedef struct {
 // Set time-out on 30 miliseconds
 const struct timeval timeout = {
   .tv_sec  =     0, // 0 second
-  .tv_usec = 300000  // 300 000 micro seconds
+  .tv_usec = 30000  // 30 000 micro seconds
 };
 
-serial_port uart_open(const char* pcPortName)
-{
-  serial_port_unix* sp = calloc(sizeof(serial_port_unix), sizeof(uint8_t));
-  if (sp == 0) return INVALID_SERIAL_PORT;
+serial_port uart_open(const char* pcPortName) {
+	serial_port_unix* sp = calloc(sizeof(serial_port_unix), sizeof(uint8_t));
+	if (sp == 0) return INVALID_SERIAL_PORT;
 
-  if (memcmp(pcPortName, "tcp:", 4) == 0) {
-    struct addrinfo *addr, *rp;
-    char *addrstr = strdup(pcPortName + 4);
+	if (memcmp(pcPortName, "tcp:", 4) == 0) {
+		struct addrinfo *addr, *rp;
+		char *addrstr = strdup(pcPortName + 4);
 
-    if (addrstr == NULL) {
-      printf("Error: strdup\n");
-      return INVALID_SERIAL_PORT;
-    }
+		if (addrstr == NULL) {
+			printf("Error: strdup\n");
+			return INVALID_SERIAL_PORT;
+		}
 
-    char *colon = strrchr(addrstr, ':');
-    char *portstr;
-    if (colon) {
-      portstr = colon + 1;
-      *colon = '\0';
-    } else {
-      portstr = "7901";
+		timeout.tv_usec = 300000  // 300 000 micro seconds
+		
+		char *colon = strrchr(addrstr, ':');
+		char *portstr;
+		if (colon) {
+			portstr = colon + 1;
+			*colon = '\0';
+		} else {
+			portstr = "7901";
+		}
+
+		int s = getaddrinfo(addrstr, portstr, NULL, &addr);
+		if (s != 0) {
+			printf("Error: getaddrinfo: %s\n", gai_strerror(s));
+			return INVALID_SERIAL_PORT;
+		}
+
+		int sfd;
+		for (rp = addr; rp != NULL; rp = rp->ai_next) {
+			sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+
+			if (sfd == -1)
+				continue;
+
+			if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+				break;
+
+			close(sfd);
+		}
+
+		if (rp == NULL) {               /* No address succeeded */
+			printf("Error: Could not connect\n");
+			return INVALID_SERIAL_PORT;
+		}
+
+		freeaddrinfo(addr);
+		free(addrstr);
+
+		sp->fd = sfd;
+
+		int one = 1;
+		setsockopt(sp->fd, SOL_TCP, TCP_NODELAY, &one, sizeof(one));
+		return sp;
 	}
-	
-	int s = getaddrinfo(addrstr, portstr, NULL, &addr);
-    if (s != 0) {
-      printf("Error: getaddrinfo: %s\n", gai_strerror(s));
-      return INVALID_SERIAL_PORT;
-    }
-
-    int sfd;
-    for (rp = addr; rp != NULL; rp = rp->ai_next) {
-      sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-
-      if (sfd == -1)
-		continue;
-	
-      if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-		break;
-	
-      close(sfd);
-    }
-	
-    if (rp == NULL) {               /* No address succeeded */
-      printf("Error: Could not connect\n");
-      return INVALID_SERIAL_PORT;
-    }
-	
-    freeaddrinfo(addr);
-    free(addrstr);
-	
-    sp->fd = sfd;
-
-    int one = 1;
-    setsockopt(sp->fd, SOL_TCP, TCP_NODELAY, &one, sizeof(one));
-    return sp;
-  }
 
   
-  sp->fd = open(pcPortName, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
-  if(sp->fd == -1) {
-    uart_close(sp);
-    return INVALID_SERIAL_PORT;
-  }
+	sp->fd = open(pcPortName, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
+	if(sp->fd == -1) {
+		uart_close(sp);
+		return INVALID_SERIAL_PORT;
+	}
 
-  // Finally figured out a way to claim a serial port interface under unix
-  // We just try to set a (advisory) lock on the file descriptor
-  struct flock fl;
-  fl.l_type   = F_WRLCK;
-  fl.l_whence = SEEK_SET;
-  fl.l_start  = 0;
-  fl.l_len    = 0;
-  fl.l_pid    = getpid();
-  
-  // Does the system allows us to place a lock on this file descriptor
-  if (fcntl(sp->fd, F_SETLK, &fl) == -1) {
-    // A conflicting lock is held by another process
-    free(sp);
-    return CLAIMED_SERIAL_PORT;
-  }
+	// Finally figured out a way to claim a serial port interface under unix
+	// We just try to set a (advisory) lock on the file descriptor
+	struct flock fl;
+	fl.l_type   = F_WRLCK;
+	fl.l_whence = SEEK_SET;
+	fl.l_start  = 0;
+	fl.l_len    = 0;
+	fl.l_pid    = getpid();
 
-  // Try to retrieve the old (current) terminal info struct
-  if(tcgetattr(sp->fd,&sp->tiOld) == -1) {
-    uart_close(sp);
-    return INVALID_SERIAL_PORT;
-  }
-  
-  // Duplicate the (old) terminal info struct
-  sp->tiNew = sp->tiOld;
-  
-  // Configure the serial port
-  sp->tiNew.c_cflag = CS8 | CLOCAL | CREAD;
-  sp->tiNew.c_iflag = IGNPAR;
-  sp->tiNew.c_oflag = 0;
-  sp->tiNew.c_lflag = 0;
-    
-  // Block until n bytes are received
-  sp->tiNew.c_cc[VMIN] = 0;
-  // Block until a timer expires (n * 100 mSec.)
-  sp->tiNew.c_cc[VTIME] = 0;
-  
-  // Try to set the new terminal info struct
-  if(tcsetattr(sp->fd, TCSANOW, &sp->tiNew) == -1) {
-    uart_close(sp);
-    return INVALID_SERIAL_PORT;
-  }
-  
+	// Does the system allows us to place a lock on this file descriptor
+	if (fcntl(sp->fd, F_SETLK, &fl) == -1) {
+		// A conflicting lock is held by another process
+		free(sp);
+		return CLAIMED_SERIAL_PORT;
+	}
+
+	// Try to retrieve the old (current) terminal info struct
+	if(tcgetattr(sp->fd,&sp->tiOld) == -1) {
+		uart_close(sp);
+		return INVALID_SERIAL_PORT;
+	}
+
+	// Duplicate the (old) terminal info struct
+	sp->tiNew = sp->tiOld;
+
+	// Configure the serial port
+	sp->tiNew.c_cflag = CS8 | CLOCAL | CREAD;
+	sp->tiNew.c_iflag = IGNPAR;
+	sp->tiNew.c_oflag = 0;
+	sp->tiNew.c_lflag = 0;
+
+	// Block until n bytes are received
+	sp->tiNew.c_cc[VMIN] = 0;
+	// Block until a timer expires (n * 100 mSec.)
+	sp->tiNew.c_cc[VTIME] = 0;
+
+	// Try to set the new terminal info struct
+	if(tcsetattr(sp->fd, TCSANOW, &sp->tiNew) == -1) {
+		uart_close(sp);
+		return INVALID_SERIAL_PORT;
+	}
+
   // Flush all lingering data that may exist
   tcflush(sp->fd, TCIOFLUSH);
 
 #ifdef WITH_FPC
 	if ( uart_set_speed(sp, 115200) ) {
-  printf("[=] UART Setting serial baudrate 115200 [FPC enabled]\n");  
+		printf("[=] UART Setting serial baudrate 115200 [FPC enabled]\n");  
 	} else {
 		uart_set_speed(sp, 9600);
 		printf("[=] UART Setting serial baudrate 9600 [FPC enabled]\n");		
 	}
 #else	
-  // set speed, works for UBUNTU 14.04
-  bool success = uart_set_speed(sp, 460800);
-  if (success) {
-    printf("[=] UART Setting serial baudrate 460800\n");
-  } else {
-    uart_set_speed(sp, 115200);
-    printf("[=] UART Setting serial baudrate 115200\n");
-  }
+	// set speed, works for UBUNTU 14.04
+	bool success = uart_set_speed(sp, 460800);
+	if (success) {
+		printf("[=] UART Setting serial baudrate 460800\n");
+	} else {
+		uart_set_speed(sp, 115200);
+		printf("[=] UART Setting serial baudrate 115200\n");
+	}
 #endif  
-  return sp;
+	return sp;
 }
 
 void uart_close(const serial_port sp) {
-  serial_port_unix* spu = (serial_port_unix*)sp;
-  tcflush(spu->fd, TCIOFLUSH);
-  tcsetattr(spu->fd, TCSANOW, &(spu->tiOld));
-  struct flock fl;
-  fl.l_type   = F_UNLCK;
-  fl.l_whence = SEEK_SET;
-  fl.l_start  = 0;
-  fl.l_len    = 0;
-  fl.l_pid    = getpid();
+	serial_port_unix* spu = (serial_port_unix*)sp;
+	tcflush(spu->fd, TCIOFLUSH);
+	tcsetattr(spu->fd, TCSANOW, &(spu->tiOld));
+	struct flock fl;
+	fl.l_type   = F_UNLCK;
+	fl.l_whence = SEEK_SET;
+	fl.l_start  = 0;
+	fl.l_len    = 0;
+	fl.l_pid    = getpid();
 
-  // Does the system allows us to place a lock on this file descriptor
-  int err = fcntl(spu->fd, F_SETLK, &fl);
-  if ( err == -1) {
-     //perror("fcntl");
-  }  
-  close(spu->fd);
-  free(sp);
+	// Does the system allows us to place a lock on this file descriptor
+	int err = fcntl(spu->fd, F_SETLK, &fl);
+	if ( err == -1) {
+		//perror("fcntl");
+	}  
+	close(spu->fd);
+	free(sp);
 }
 
 bool uart_receive(const serial_port sp, uint8_t* pbtRx, size_t pszMaxRxLen, size_t* pszRxLen) {
-  int res;
-  int byteCount;
-  fd_set rfds;
-  struct timeval tv;
+	int res;
+	int byteCount;
+	fd_set rfds;
+	struct timeval tv;
+
+	// Reset the output count
+	*pszRxLen = 0;
   
-  // Reset the output count
-  *pszRxLen = 0;
-  
-  do {
-    // Reset file descriptor
-    FD_ZERO(&rfds);
-    FD_SET(((serial_port_unix*)sp)->fd, &rfds);
-    tv = timeout;
-    res = select(((serial_port_unix*)sp)->fd + 1, &rfds, NULL, NULL, &tv);
-    
-    // Read error
-    if (res < 0) {
-      return false;
-    }
- 
-    // Read time-out
-    if (res == 0) {
-      if (*pszRxLen == 0) {
-        // Error, we received no data
-        return false;
-      } else {
-        // We received some data, but nothing more is available
-        return true;
-      }
-    }
- 
-    // Retrieve the count of the incoming bytes
-    res = ioctl(((serial_port_unix*)sp)->fd, FIONREAD, &byteCount);
-    if (res < 0) return false;
-    
-    // Cap the number of bytes, so we don't overrun the buffer
-    if (pszMaxRxLen - (*pszRxLen) < byteCount) {
-    	byteCount = pszMaxRxLen - (*pszRxLen);
-    }
+	do {
+		// Reset file descriptor
+		FD_ZERO(&rfds);
+		FD_SET(((serial_port_unix*)sp)->fd, &rfds);
+		tv = timeout;
+		res = select(((serial_port_unix*)sp)->fd + 1, &rfds, NULL, NULL, &tv);
 
-    // There is something available, read the data
-    res = read(((serial_port_unix*)sp)->fd, pbtRx + (*pszRxLen), byteCount);
+		// Read error
+		if (res < 0) {
+			return false;
+		}
 
-    // Stop if the OS has some troubles reading the data
-    if (res <= 0) return false;
- 
-    *pszRxLen += res;
+		// Read time-out
+		if (res == 0) {
+			if (*pszRxLen == 0) {
+				// Error, we received no data
+				return false;
+			} else {
+				// We received some data, but nothing more is available
+				return true;
+			}
+		}
 
-    if (*pszRxLen == pszMaxRxLen) {
-    	// We have all the data we wanted.
-      return true;
-    }
-    
-  } while (byteCount);
+		// Retrieve the count of the incoming bytes
+		res = ioctl(((serial_port_unix*)sp)->fd, FIONREAD, &byteCount);
+		if (res < 0) return false;
 
-  return true;
+		// Cap the number of bytes, so we don't overrun the buffer
+		if (pszMaxRxLen - (*pszRxLen) < byteCount) {
+			byteCount = pszMaxRxLen - (*pszRxLen);
+		}
+
+		// There is something available, read the data
+		res = read(((serial_port_unix*)sp)->fd, pbtRx + (*pszRxLen), byteCount);
+
+		// Stop if the OS has some troubles reading the data
+		if (res <= 0) return false;
+
+		*pszRxLen += res;
+
+		if (*pszRxLen == pszMaxRxLen) {
+			// We have all the data we wanted.
+			return true;
+		}
+
+	} while (byteCount);
+
+	return true;
 }
 
 bool uart_send(const serial_port sp, const uint8_t* pbtTx, const size_t len) {
-  int32_t res;
-  size_t pos = 0;
-  fd_set rfds;
-  struct timeval tv;
+	int32_t res;
+	size_t pos = 0;
+	fd_set rfds;
+	struct timeval tv;
 
-  while (pos < len) {
-    // Reset file descriptor
-    FD_ZERO(&rfds);
-    FD_SET(((serial_port_unix*)sp)->fd, &rfds);
-    tv = timeout;
-    res = select(((serial_port_unix*)sp)->fd+1, NULL, &rfds, NULL, &tv);
-    
-    // Write error
-    if (res < 0) {
-		printf("UART:: write error (%d)\n", res);
-		return false;
-    }
-    
-    // Write time-out
-    if (res == 0) {
-		printf("UART:: write time-out\n");
-		return false;
-    }
-    
-    // Send away the bytes
-    res = write(((serial_port_unix*)sp)->fd, pbtTx + pos, len - pos);
-    
-    // Stop if the OS has some troubles sending the data
-    if (res <= 0) return false;
-    
-    pos += res;
-  }
-  return true;
+	while (pos < len) {
+		// Reset file descriptor
+		FD_ZERO(&rfds);
+		FD_SET(((serial_port_unix*)sp)->fd, &rfds);
+		tv = timeout;
+		res = select(((serial_port_unix*)sp)->fd+1, NULL, &rfds, NULL, &tv);
+
+		// Write error
+		if (res < 0) {
+			printf("UART:: write error (%d)\n", res);
+			return false;
+		}
+
+		// Write time-out
+		if (res == 0) {
+			printf("UART:: write time-out\n");
+			return false;
+		}
+
+		// Send away the bytes
+		res = write(((serial_port_unix*)sp)->fd, pbtTx + pos, len - pos);
+
+		// Stop if the OS has some troubles sending the data
+		if (res <= 0) return false;
+
+		pos += res;
+	}
+	return true;
 }
 
 bool uart_set_speed(serial_port sp, const uint32_t uiPortSpeed) {
-  const serial_port_unix* spu = (serial_port_unix*)sp;
-  speed_t stPortSpeed;
-  switch (uiPortSpeed) {
-    case 0: stPortSpeed = B0; break;
-    case 50: stPortSpeed = B50; break;
-    case 75: stPortSpeed = B75; break;
-    case 110: stPortSpeed = B110; break;
-    case 134: stPortSpeed = B134; break;
-    case 150: stPortSpeed = B150; break;
-    case 300: stPortSpeed = B300; break;
-    case 600: stPortSpeed = B600; break;
-    case 1200: stPortSpeed = B1200; break;
-    case 1800: stPortSpeed = B1800; break;
-    case 2400: stPortSpeed = B2400; break;
-    case 4800: stPortSpeed = B4800; break;
-    case 9600: stPortSpeed = B9600; break;
-    case 19200: stPortSpeed = B19200; break;
-    case 38400: stPortSpeed = B38400; break;
+	const serial_port_unix* spu = (serial_port_unix*)sp;
+	speed_t stPortSpeed;
+	switch (uiPortSpeed) {
+		case 0: stPortSpeed = B0; break;
+		case 50: stPortSpeed = B50; break;
+		case 75: stPortSpeed = B75; break;
+		case 110: stPortSpeed = B110; break;
+		case 134: stPortSpeed = B134; break;
+		case 150: stPortSpeed = B150; break;
+		case 300: stPortSpeed = B300; break;
+		case 600: stPortSpeed = B600; break;
+		case 1200: stPortSpeed = B1200; break;
+		case 1800: stPortSpeed = B1800; break;
+		case 2400: stPortSpeed = B2400; break;
+		case 4800: stPortSpeed = B4800; break;
+		case 9600: stPortSpeed = B9600; break;
+		case 19200: stPortSpeed = B19200; break;
+		case 38400: stPortSpeed = B38400; break;
 #  ifdef B57600
-    case 57600: stPortSpeed = B57600; break;
+		case 57600: stPortSpeed = B57600; break;
 #  endif
 #  ifdef B115200
-    case 115200: stPortSpeed = B115200; break;
+		case 115200: stPortSpeed = B115200; break;
 #  endif
 #  ifdef B230400
-    case 230400: stPortSpeed = B230400; break;
+		case 230400: stPortSpeed = B230400; break;
 #  endif
 #  ifdef B460800
-    case 460800: stPortSpeed = B460800; break;
+		case 460800: stPortSpeed = B460800; break;
 #  endif
 #  ifdef B921600
-    case 921600: stPortSpeed = B921600; break;
+		case 921600: stPortSpeed = B921600; break;
 #  endif
-    default: return false;
-  };
-  struct termios ti;
-  if (tcgetattr(spu->fd,&ti) == -1) return false;
-  // Set port speed (Input and Output)
-  cfsetispeed(&ti, stPortSpeed);
-  cfsetospeed(&ti, stPortSpeed);
-  return (tcsetattr(spu->fd, TCSANOW, &ti) != -1);
+		default: return false;
+	};
+	struct termios ti;
+	if (tcgetattr(spu->fd,&ti) == -1) return false;
+	// Set port speed (Input and Output)
+	cfsetispeed(&ti, stPortSpeed);
+	cfsetospeed(&ti, stPortSpeed);
+	return (tcsetattr(spu->fd, TCSANOW, &ti) != -1);
 }
 
 uint32_t uart_get_speed(const serial_port sp) {
-  struct termios ti;
-  uint32_t uiPortSpeed;
-  const serial_port_unix* spu = (serial_port_unix*)sp;
-  if (tcgetattr(spu->fd, &ti) == -1) return 0;
-  // Set port speed (Input)
-  speed_t stPortSpeed = cfgetispeed(&ti);
-  switch (stPortSpeed) {
-    case B0: uiPortSpeed = 0; break;
-    case B50: uiPortSpeed = 50; break;
-    case B75: uiPortSpeed = 75; break;
-    case B110: uiPortSpeed = 110; break;
-    case B134: uiPortSpeed = 134; break;
-    case B150: uiPortSpeed = 150; break;
-    case B300: uiPortSpeed = 300; break;
-    case B600: uiPortSpeed = 600; break;
-    case B1200: uiPortSpeed = 1200; break;
-    case B1800: uiPortSpeed = 1800; break;
-    case B2400: uiPortSpeed = 2400; break;
-    case B4800: uiPortSpeed = 4800; break;
-    case B9600: uiPortSpeed = 9600; break;
-    case B19200: uiPortSpeed = 19200; break;
-    case B38400: uiPortSpeed = 38400; break;
+	struct termios ti;
+	uint32_t uiPortSpeed;
+	const serial_port_unix* spu = (serial_port_unix*)sp;
+	
+	if (tcgetattr(spu->fd, &ti) == -1) 
+		return 0;
+	
+	// Set port speed (Input)
+	speed_t stPortSpeed = cfgetispeed(&ti);
+	switch (stPortSpeed) {
+		case B0: uiPortSpeed = 0; break;
+		case B50: uiPortSpeed = 50; break;
+		case B75: uiPortSpeed = 75; break;
+		case B110: uiPortSpeed = 110; break;
+		case B134: uiPortSpeed = 134; break;
+		case B150: uiPortSpeed = 150; break;
+		case B300: uiPortSpeed = 300; break;
+		case B600: uiPortSpeed = 600; break;
+		case B1200: uiPortSpeed = 1200; break;
+		case B1800: uiPortSpeed = 1800; break;
+		case B2400: uiPortSpeed = 2400; break;
+		case B4800: uiPortSpeed = 4800; break;
+		case B9600: uiPortSpeed = 9600; break;
+		case B19200: uiPortSpeed = 19200; break;
+		case B38400: uiPortSpeed = 38400; break;
 #  ifdef B57600
-    case B57600: uiPortSpeed = 57600; break;
+		case B57600: uiPortSpeed = 57600; break;
 #  endif
 #  ifdef B115200
-    case B115200: uiPortSpeed = 115200; break;
+		case B115200: uiPortSpeed = 115200; break;
 #  endif
 #  ifdef B230400
-    case B230400: uiPortSpeed = 230400; break;
+		case B230400: uiPortSpeed = 230400; break;
 #  endif
 #  ifdef B460800
-    case B460800: uiPortSpeed = 460800; break;
+		case B460800: uiPortSpeed = 460800; break;
 #  endif
 #  ifdef B921600
-    case B921600: uiPortSpeed = 921600; break;
+		case B921600: uiPortSpeed = 921600; break;
 #  endif
-    default: return 0;
-  };
-  return uiPortSpeed;
+		default: return 0;
+	};
+	return uiPortSpeed;
 }
-
 #endif
