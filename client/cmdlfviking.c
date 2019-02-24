a47085a7b4461ff15119596474c69cce48eb5ae1
iceman1001 <iceman@iuse.se>
2019-02-24 17:30:55 +0100

    text

--- client/cmdlfviking.c
+++ client/cmdlfviking.c
@@ -118,7 +118,7 @@ int CmdVikingClone(const char *Cmd) {
 
 	rawID = getVikingBits(id);
 	
-	PrintAndLogEx(INFO, "Cloning - ID: %08X, Raw: %08X%08X",id,(uint32_t)(rawID >> 32),(uint32_t) (rawID & 0xFFFFFFFF));
+	PrintAndLogEx(INFO, "Preparing to clone Viking tag - ID: %08X, Raw: %08X%08X",id,(uint32_t)(rawID >> 32),(uint32_t) (rawID & 0xFFFFFFFF));
 	
 	UsbCommand c = {CMD_VIKING_CLONE_TAG, {rawID >> 32, rawID & 0xFFFFFFFF, Q5}};
 	clearCommandBuffer();
