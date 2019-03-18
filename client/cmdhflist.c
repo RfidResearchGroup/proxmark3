52b90d3d6b85cff6cb895fda0f9f2ca1a5f39eb2
iceman1001 <iceman@iuse.se>
2019-03-18 21:24:10 +0100

    bad comparission

--- client/cmdhflist.c
+++ client/cmdhflist.c
@@ -44,7 +44,7 @@ void ClearAuthData() {
 
 uint8_t iso14443A_CRC_check(bool isResponse, uint8_t *d, uint8_t n) {
     if (n < 3) return 2;
-    if (isResponse & (n < 6)) return 2;
+    if (isResponse && (n < 6)) return 2;
     return check_crc(CRC_14443_A, d, n);
 }
 
