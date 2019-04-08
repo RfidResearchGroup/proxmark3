2b3e5b7aac6a5c50ad8046abcf98b202e1d60d99
iceman1001 <iceman@iuse.se>
2019-04-08 10:19:01 +0200

    style

--- client/emv/tlv.c
+++ client/emv/tlv.c
@@ -508,7 +508,7 @@ unsigned char *tlv_encode(const struct tlv *tlv, size_t *len) {
 }
 
 bool tlv_is_constructed(const struct tlv *tlv) {
-    return (tlv->tag < 0x100 ? tlv->tag : tlv->tag >> 8) & TLV_TAG_COMPLEX;
+    return (((tlv->tag < 0x100 ? tlv->tag : tlv->tag >> 8) & TLV_TAG_COMPLEX) == TLV_TAG_COMPLEX);
 }
 
 bool tlv_equal(const struct tlv *a, const struct tlv *b) {
