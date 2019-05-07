7ccff2db4adfc0bbf39d5f513d4403695681d818
iceman1001 <iceman@iuse.se>
2019-05-07 23:19:22 +0200

    textual

--- client/scripts/emul2html.lua
+++ client/scripts/emul2html.lua
@@ -4,12 +4,18 @@ getopt = require('getopt')
 bin = require('bin')
 dumplib = require('html_dumplib')
 
-example = "script run emul2html -o dumpdata.eml "
-author = "Martin Holst Swende"
-usage = "script run htmldump [-i <file>] [-o <file>]"
-desc =[[
+copyright = ''
+author = 'Martin Holst Swende'
+version = 'v1.0.1'
+desc = [[
 This script takes a dumpfile on EML (ASCII) format and produces a html based dump, which is a
 bit more easily analyzed.
+]]
+example = [[
+    script run emul2html -o dumpdata.eml
+]]
+usage = [[
+script run htmldump [-i <file>] [-o <file>]
 
 Arguments:
     -h              This help
@@ -18,45 +24,54 @@ Arguments:
 
 ]]
 
--------------------------------
--- Some utilities
--------------------------------
-
+-- Some globals
+local DEBUG = false -- the debug flag
 ---
 -- A debug printout-function
-function dbg(args)
-    if DEBUG then
-        print("###", args)
+local function dbg(args)
+    if not DEBUG then return end
+    if type(args) == 'table' then
+        local i = 1
+        while args[i] do
+            dbg(args[i])
+            i = i+1
+        end
+    else
+        print('###', args)
     end
 end
 ---
 -- This is only meant to be used when errors occur
-function oops(err)
-    print("ERROR: ",err)
+local function oops(err)
+    print('ERROR:', err)
+    core.clearCommandBuffer()
+    return nil, err
 end
-
-
 ---
 -- Usage help
-function help()
+local function help()
+    print(copyright)
+    print(author)
+    print(version)
     print(desc)
-    print("Example usage")
+    print('Example usage')
     print(example)
+    print(usage)
 end
 
 local function main(args)
 
-    local input = "dumpdata.eml"
-    local output = os.date("%Y-%m-%d_%H%M%S.html");
+    local input = 'dumpdata.eml'
+    local output = os.date('%Y-%m-%d_%H%M%S.html');
     for o, a in getopt.getopt(args, 'i:o:h') do
-        if o == "h" then return help() end
-        if o == "i" then input = a end
-        if o == "o" then output = a end
+        if o == 'h' then return help() end
+        if o == 'i' then input = a end
+        if o == 'o' then output = a end
     end
     local filename, err = dumplib.convert_eml_to_html(input,output)
     if err then return oops(err) end
 
-    print(("Wrote a HTML dump to the file %s"):format(filename))
+    print(('Wrote a HTML dump to the file %s'):format(filename))
 end
 
 --[[
