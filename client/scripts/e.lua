9c95a2e8aa471d9ddb1a63a1a36e1df784b37136
iceman1001 <iceman@iuse.se>
2019-05-07 23:37:15 +0200

    textual

--- client/scripts/e.lua
+++ client/scripts/e.lua
@@ -1,39 +1,52 @@
 local getopt = require('getopt')
 local utils = require('utils')
 
-example = "script calculates many different checksums (CRC) over the provided hex input"
-author = "Iceman"
-desc =
-[[
+copyright = ''
+author = 'Iceman'
+version = 'v1.0.1'
+desc = [[
 This script calculates many checksums (CRC) over the provided hex input.
-
-Arguments:
-    -b              data in hex
-    -w              bitwidth of the CRC family of algorithm. <optional> defaults to all known CRC presets.
-Examples :
+]]
+example = [[
     script run e -b 010203040506070809
     script run e -b 010203040506070809 -w 16
 ]]
-
+usage = [[
+Arguments:
+     -b       data in hex
+     -w       bitwidth of the CRC family of algorithm. <optional> defaults to all known CRC presets.
+]]
 ---
 -- A debug printout-function
-function dbg(args)
-    if DEBUG then
-        print("###", args)
+local function dbg(args)
+    if not DEBUG then return end
+    if type(args) == 'table' then
+        local i = 1
+        while args[i] do
+            dbg(args[i])
+            i = i+1
+        end
+    else
+        print('###', args)
     end
 end
 ---
 -- This is only meant to be used when errors occur
-function oops(err)
-    print("[!] ERROR: ",err)
-    return nil,err
+local function oops(err)
+    print('ERROR:', err)
+    core.clearCommandBuffer()
+    return nil, err
 end
 ---
 -- Usage help
-function help()
+local function help()
+    print(copyright)
+    print(author)
+    print(version)
     print(desc)
-    print("Example usage")
+    print('Example usage')
     print(example)
+    print(usage)
 end
 ---
 -- The main entry point
@@ -44,9 +57,9 @@ function main(args)
 
     -- Read the parameters
     for o, a in getopt.getopt(args, 'hb:w:') do
-        if o == "h" then return help() end
-        if o == "b" then data = a end
-        if o == "w" then width = a end
+        if o == 'h' then return help() end
+        if o == 'b' then data = a end
+        if o == 'w' then width = a end
     end
 
     data = data or '01020304'
