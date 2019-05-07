5b087a053c71cce274acc44daadeceefcf457107
iceman1001 <iceman@iuse.se>
2019-05-07 23:53:06 +0200

    fix: script run legic_buffer2card -  correct params for write command

--- client/scripts/legic_buffer2card.lua
+++ client/scripts/legic_buffer2card.lua
@@ -1,32 +1,41 @@
--- this script writes bytes 8 to 256 on the Legic MIM256
-example = "Script writes to Legic Prime Tag from position 0x07 until 0xFF with the value 0x01"
-author = "Mosci"
-desc =
-[[
-This is a script which writes value 0x01 to bytes from position 0x07 until 0xFF on a Legic Prime Tag (MIM256 or MIM1024)
-(created with 'hf legic save my_dump.hex')
-
-optional arguments :
-    -h       - Help text
-
-Examples :
-    script run legic_buffer2card
-]]
-
 local utils = require('utils')
 local getopt = require('getopt')
+-- this script writes bytes 8 to 256 on the Legic MIM256
+
+copyright = ''
+author = 'Mosci'
+version = 'v1.0.1'
+desc =
+[[
+This is a script which writes value 0x01 to bytes from position 0x07 until 0xFF on a Legic Prime Tag (MIM256 or MIM1024)  -- (created with 'hf legic save my_dump.hex') --
+]]
+example = [[
+    script run legic_buffer2card
+]]
+usage = [[
+script run legic_buffer2card -h
+
+Arguments
+    -h       - Help text
+]]
+
 ---
 -- This is only meant to be used when errors occur
-function oops(err)
-    print("ERROR: ",err)
+local function oops(err)
+    print('ERROR:', err)
+    core.clearCommandBuffer()
     return nil, err
-   end
+end
 ---
 -- Usage help
-function help()
+local function help()
+    print(copyright)
+    print(author)
+    print(version)
     print(desc)
-    print("Example usage")
+    print('Example usage')
     print(example)
+    print(usage)
 end
 --
 -- simple loop-write from 0x07 to 0xff
@@ -34,13 +43,13 @@ function main()
 
     -- parse arguments for the script
     for o, a in getopt.getopt(args, 'h') do
-        if o == "h" then return help() end
+        if o == 'h' then return help() end
     end
 
     local cmd = ''
     local i
     for i = 7, 255 do
-        cmd = ('hf legic write 0x%02x 0x01'):format(i)
+        cmd = ('hf legic write o %02x d 01'):format(i)
         print(cmd)
         core.clearCommandBuffer()
         core.console(cmd)
