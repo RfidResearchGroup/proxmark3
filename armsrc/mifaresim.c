f6f7e7a9f62de7ef37e44b2001a50f4c6900219b
Eloff <eloff@mail.ru>
2019-07-14 23:12:39 +0300

    hf_mf_sim. Really tags not respond NACK on invalid authentication

--- armsrc/mifaresim.c
+++ armsrc/mifaresim.c
@@ -1036,8 +1036,9 @@ void Mifare1ksim(uint16_t flags, uint8_t exitAfterNReads, uint8_t *datain) {
                                 );
                     }
                     cardAUTHKEY = AUTHKEYNONE;	// not authenticated
-                    EmSend4bit(mf_crypto1_encrypt4bit(pcs, CARD_NACK_NA));
                     cardSTATE_TO_IDLE();
+                    // Really tags not respond NACK on invalid authentication
+                    LogTrace(uart->output, uart->len, uart->startTime * 16 - DELAY_AIR2ARM_AS_TAG, uart->endTime * 16 - DELAY_AIR2ARM_AS_TAG, uart->parity, true);
                     break;
                 }
 
