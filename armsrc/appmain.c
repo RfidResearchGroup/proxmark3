c74028fa10ddbeb8a567846a7f261d45602dc331
Philippe Teuwen <phil@teuwen.org>
2019-08-06 13:42:17 +0200

    show slow clock in hw status

--- armsrc/appmain.c
+++ armsrc/appmain.c
@@ -431,6 +431,9 @@ void SendStatus(void) {
     Dbprintf("  ToSendMax...............%d", ToSendMax);
     Dbprintf("  ToSendBit...............%d", ToSendBit);
     Dbprintf("  ToSend BUFFERSIZE.......%d", TOSEND_BUFFER_SIZE);
+    while ((AT91C_BASE_PMC->PMC_MCFR & AT91C_CKGR_MAINRDY) == 0);       // Wait for MAINF value to become available...
+    uint16_t mainf = AT91C_BASE_PMC->PMC_MCFR & AT91C_CKGR_MAINF;       // Get # main clocks within 16 slow clocks
+    Dbprintf("  Slow clock..............%d Hz", (16*MAINCK) / mainf);
     DbpString(_BLUE_("Installed StandAlone Mode"));
     ModInfo();
 
