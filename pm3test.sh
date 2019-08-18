9b7bff7baf7b050b9afd9f40341ffb8b7d0dd6e5
merlokk <807634+merlokk@users.noreply.github.com>
2019-08-18 18:56:30 +0300

    fix1

--- pm3test.sh
+++ pm3test.sh
@@ -37,8 +37,8 @@ function CheckExecute() {
 
 printf "\n${C_BLUE}RRG Proxmark3 test tool ${C_NC}\n\n"
 
-if [ ! $TRAVIS_PULL_REQUEST=="" ]; then
-  if [ $TRAVIS_PULL_REQUEST=="false" ]; then
+if [ ! "$TRAVIS_PULL_REQUEST"=="" ]; then
+  if [ "$TRAVIS_PULL_REQUEST"=="false" ]; then
     echo "Travis pull request: $TRAVIS_PULL_REQUEST branch: $TRAVIS_PULL_REQUEST_SLUG commit: $TRAVIS_COMMIT"
   else
     echo "Travis branch: $TRAVIS_REPO_SLUG commit: $TRAVIS_PULL_REQUEST_SHA"
@@ -50,7 +50,7 @@ git describe --all
 printf "git sha: " 
 git rev-parse HEAD
 
-while false; do
+while true; do
   if ! CheckFileExist "proxmark3 exists" "./client/proxmark3"; then break; fi
   if ! CheckFileExist "arm image exists" "./armsrc/obj/fullimage.elf"; then break; fi
   if ! CheckFileExist "bootrom exists" "./bootrom/obj/bootrom.elf"; then break; fi
