3e3ee66587de988b239a047cdfcc0cd8a44f84e3
merlokk <807634+merlokk@users.noreply.github.com>
2019-08-18 19:20:41 +0300

    x4

--- pm3test.sh
+++ pm3test.sh
@@ -39,9 +39,9 @@ printf "\n${C_BLUE}RRG Proxmark3 test tool ${C_NC}\n\n"
 
 if [ "$TRAVIS_COMMIT" ]; then
   if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "Travis branch: $TRAVIS_REPO_SLUG commit: $TRAVIS_PULL_REQUEST_SHA"
+    echo "Travis branch: $TRAVIS_BRANCH slug: $TRAVIS_REPO_SLUG commit: $TRAVIS_PULL_REQUEST_SHA"
   else
-    echo "Travis pull request: $TRAVIS_PULL_REQUEST branch: $TRAVIS_PULL_REQUEST_SLUG commit: $TRAVIS_COMMIT"
+    echo "Travis pull request: $TRAVIS_PULL_REQUEST branch: $TRAVIS_BRANCH slug: $TRAVIS_PULL_REQUEST_SLUG commit: $TRAVIS_COMMIT"
   fi
 fi
 
