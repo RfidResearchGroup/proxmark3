01950bd962e835e9d37596efeb4f8e9478eee34d
Iceman <iceman1001@iuse.se>
2019-09-05 15:46:40 +0200

    chg more ansi color

--- include/ansi.h
+++ include/ansi.h
@@ -1,11 +1,14 @@
 #ifndef __ANSI_H
 #define __ANSI_H
 
-#define _BLUE_(s) "\x1b[34m" s "\x1b[0m "
-#define _RED_(s) "\x1b[31m" s "\x1b[0m "
-#define _GREEN_(s) "\x1b[32m" s "\x1b[0m "
-#define _YELLOW_(s) "\x1b[33m" s "\x1b[0m "
-#define _MAGENTA_(s) "\x1b[35m" s "\x1b[0m "
-#define _CYAN_(s) "\x1b[36m" s "\x1b[0m "
+#define AEND  "\x1b[0m "
+
+#define _BLUE_(s) "\x1b[34m" s AEND
+#define _RED_(s) "\x1b[31m" s AEND
+#define _GREEN_(s) "\x1b[32m" s AEND
+#define _YELLOW_(s) "\x1b[33m" s AEND
+#define _MAGENTA_(s) "\x1b[35m" s AEND
+#define _CYAN_(s) "\x1b[36m" s AEND
+#define _WHITE_(s) "\x1b[37m" s AEND
 
 #endif
