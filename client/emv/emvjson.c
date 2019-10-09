f1b15d8e076fd017390a9f87cd80d0434d2b2276
iceman1001 <iceman@iuse.se>
2019-10-09 14:21:09 +0200

    coverity 226425 - printf

--- client/emv/emvjson.c
+++ client/emv/emvjson.c
@@ -76,7 +76,7 @@ int JsonSaveJsonObject(json_t *root, const char *path, json_t *value) {
 
     if (path[0] == '$') {
         if (json_path_set(root, path, value, 0, &error)) {
-            PrintAndLogEx(ERR, "ERROR: can't set json path: ", error.text);
+            PrintAndLogEx(ERR, "ERROR: can't set json path: %s", error.text);
             return 2;
         } else {
             return 0;
