9be5627537bbe620ebdd551a858d9a8b4dcf618f
iceman1001 <iceman@iuse.se>
2019-10-10 11:37:36 +0200

    should unify authors messages, remove from individual functions that was done for awhile

--- client/cmdlfcotag.h
+++ client/cmdlfcotag.h
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// Authored by Iceman
+// Iceman
 //
 // This code is licensed to you under the terms of the GNU GPL, version 2 or,
 // at your option, any later version. See the LICENSE.txt file for the text of
--- client/cmdlfgallagher.h
+++ client/cmdlfgallagher.h
@@ -1,4 +1,5 @@
 //-----------------------------------------------------------------------------
+// Iceman, 2019
 //
 // This code is licensed to you under the terms of the GNU GPL, version 2 or,
 // at your option, any later version. See the LICENSE.txt file for the text of
