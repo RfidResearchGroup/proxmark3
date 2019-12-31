855aee04ddf7e0898beeebbfa5fa88b68d566cb9
osboxes.org <chris@rfidresearch.com>
2019-12-31 21:40:12 +0100

    style

--- client/jansson/load.c
+++ client/jansson/load.c
@@ -1079,7 +1079,7 @@ static int callback_get(void *data) {
     if (stream->pos >= stream->len) {
         stream->pos = 0;
         stream->len = stream->callback(stream->data, MAX_BUF_LEN, stream->arg);
-        if (stream->len == 0 || stream->len == (size_t) -1)
+        if (stream->len == 0 || stream->len == (size_t) - 1)
             return EOF;
     }
 
