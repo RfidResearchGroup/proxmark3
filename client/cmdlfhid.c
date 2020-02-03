e2d2e86df6f4feb089ad108e4eb3f0e260193d92
iceman1001 <iceman@iuse.se>
2020-02-03 20:53:50 +0100

    textual

--- client/cmdlfhid.c
+++ client/cmdlfhid.c
@@ -342,7 +342,8 @@ static int CmdHIDClone(const char *Cmd) {
     }
 
     clearCommandBuffer();
-    SendCommandOLD(CMD_LF_HID_CLONE, hi2, hi, lo, longid, sizeof(longid));
+    SendCommandMIX(CMD_LF_HID_CLONE, hi2, hi, lo, longid, sizeof(longid));
+    PrintAndLogEx(INFO, "Clone command sent. Try "_YELLOW_("'lf hid read'") " to verify");
     return PM3_SUCCESS;
 }
 
