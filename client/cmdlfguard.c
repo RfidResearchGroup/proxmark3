bccad7b9d5a776dd0113daa4084064098855830c
iceman1001 <iceman@iuse.se>
2020-03-10 17:06:00 +0100

    hint message

--- client/cmdlfguard.c
+++ client/cmdlfguard.c
@@ -187,7 +187,10 @@ static int CmdGuardClone(const char *Cmd) {
     PrintAndLogEx(INFO, "Preparing to clone Guardall to T55x7 with Facility Code: %u, Card Number: %u", facilitycode, cardnumber);
     print_blocks(blocks,  ARRAYLEN(blocks));
 
-    return clone_t55xx_tag(blocks, ARRAYLEN(blocks));
+    int res = clone_t55xx_tag(blocks, ARRAYLEN(blocks));
+    PrintAndLogEx(SUCCESS, "Done");
+    PrintAndLogEx(INFO, "Hint: try " _YELLOW_("`lf gprox read`") "to verify");
+    return res;
 }
 
 static int CmdGuardSim(const char *Cmd) {
