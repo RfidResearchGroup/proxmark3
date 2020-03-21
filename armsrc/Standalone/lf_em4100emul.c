c36d86bc010a50537d6e0a6b265ea1ee09c07c43
Artem Gnatyuk <temskiy@gmail.com>
2020-03-21 21:39:30 +0700

    Make style

--- armsrc/Standalone/lf_em4100emul.c
+++ armsrc/Standalone/lf_em4100emul.c
@@ -23,36 +23,36 @@
 #define CLOCK 64 //for 125kHz
 
 // low & high - array for storage IDs. Its length must be equal.
-// Predefined IDs must be stored in low[]. 
+// Predefined IDs must be stored in low[].
 // In high[] must be nulls
-uint64_t low[] = {0x565A1140BE,0x365A398149,0x5555555555,0xFFFFFFFFFF};
-uint32_t high[] = {0,0,0,0};
-uint8_t *bba,slots_count;
+uint64_t low[] = {0x565A1140BE, 0x365A398149, 0x5555555555, 0xFFFFFFFFFF};
+uint32_t high[] = {0, 0, 0, 0};
+uint8_t *bba, slots_count;
 int buflen;
 
 void ModInfo(void) {
     DbpString("  LF EM4100 simulator standalone mode");
 }
 
-uint64_t ReversQuads(uint64_t bits){
-  uint64_t result = 0;
-  for (int i = 0; i < 16; i++){
-    result += ((bits >> (60 - 4 *i)) & 0xf) << (4 * i);
-      }
-  return result >> 24;
+uint64_t ReversQuads(uint64_t bits) {
+    uint64_t result = 0;
+    for (int i = 0; i < 16; i++) {
+        result += ((bits >> (60 - 4 * i)) & 0xf) << (4 * i);
+    }
+    return result >> 24;
 }
 
 void FillBuff(uint8_t bit) {
-    memset (bba + buflen, bit, CLOCK / 2);
-	buflen += (CLOCK / 2);
-	memset (bba + buflen, bit^1,CLOCK / 2);
-	buflen += (CLOCK / 2);
+    memset(bba + buflen, bit, CLOCK / 2);
+    buflen += (CLOCK / 2);
+    memset(bba + buflen, bit ^ 1, CLOCK / 2);
+    buflen += (CLOCK / 2);
 }
 
 void ConstructEM410xEmulBuf(uint64_t id) {
-	
+
     int i, j, binary[4], parity[4];
- 	buflen = 0;
+    buflen = 0;
     for (i = 0; i < 9; i++)
         FillBuff(1);
     parity[0] = parity[1] = parity[2] = parity[3] = 0;
@@ -60,39 +60,39 @@ void ConstructEM410xEmulBuf(uint64_t id) {
         for (j = 3; j >= 0; j--, id /= 2)
             binary[j] = id % 2;
         for (j = 0; j < 4; j++)
-			FillBuff(binary[j]);
+            FillBuff(binary[j]);
         FillBuff(binary[0] ^ binary[1] ^ binary[2] ^ binary[3]);
         for (j = 0; j < 4; j++)
-			parity[j] ^= binary[j];
+            parity[j] ^= binary[j];
     }
-	for (j = 0; j < 4; j++)
-    	FillBuff(parity[j]);
+    for (j = 0; j < 4; j++)
+        FillBuff(parity[j]);
     FillBuff(0);
 }
 
 void LED_Slot(int i) {
-	LEDsoff();
-	if (slots_count > 4) {
-		LED(i % MAX_IND, 0); //binary indication for slots_count > 4
-	} else {
-		LED(1 << i,0); //simple indication for slots_count <=4
-	}
+    LEDsoff();
+    if (slots_count > 4) {
+        LED(i % MAX_IND, 0); //binary indication for slots_count > 4
+    } else {
+        LED(1 << i, 0); //simple indication for slots_count <=4
+    }
 }
 
 void RunMod() {
     StandAloneMode();
     FpgaDownloadAndGo(FPGA_BITSTREAM_LF);
-	int selected = 0; //selected slot after start
-	slots_count = sizeof(low)/sizeof(low[0]);
-	bba = BigBuf_get_addr();
-	for (;;) {		
-		WDT_HIT();
+    int selected = 0; //selected slot after start
+    slots_count = sizeof(low) / sizeof(low[0]);
+    bba = BigBuf_get_addr();
+    for (;;) {
+        WDT_HIT();
         if (data_available()) break;
-		SpinDelay(100);
-		SpinUp(100);
-		LED_Slot(selected);
-		ConstructEM410xEmulBuf(ReversQuads(low[selected]));
-		SimulateTagLowFrequency(buflen, 0, true);
-		selected = (selected + 1) % slots_count;
-	}
+        SpinDelay(100);
+        SpinUp(100);
+        LED_Slot(selected);
+        ConstructEM410xEmulBuf(ReversQuads(low[selected]));
+        SimulateTagLowFrequency(buflen, 0, true);
+        selected = (selected + 1) % slots_count;
+    }
 }
