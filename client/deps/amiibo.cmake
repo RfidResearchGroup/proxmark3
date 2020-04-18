397f46c0efad7579fbdb598c0b8747c8a65d9b16
Philippe Teuwen <phil@teuwen.org>
2020-04-18 11:39:15 +0200

    cmake: Fix the (not yet used) amiibo srcs

--- client/deps/amiibo.cmake
+++ client/deps/amiibo.cmake
@@ -6,23 +6,11 @@
 #-o amiitool
 
 set_property(SOURCE PROPERTY C_STANDARD 99)
-add_definitions(-D_ISOC99_SOURCE)
-include_directories(jansson)
-include_directories(../../common)
-include_directories(../../common/include)
-include_directories(amiitool)
 
 add_library(amiibo
-        jansson/dump.c
-        jansson/error.c
-        jansson/hashtable.c
-        jansson/hashtable_seed.c
-        jansson/load.c
-        jansson/memory.c
-        jansson/pack_unpack.c
-        jansson/strbuffer.c
-        jansson/strconv.c
-        jansson/utf.c
-        jansson/path.c
-        jansson/value.c
+        amiitool/amiibo.c
+        amiitool/drbg.c
+        amiitool/keygen.c
 )
+
+target_include_directories(amiibo PRIVATE ../../include ../../common)
