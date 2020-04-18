0b68f3b81e70a489686e2ea3c4d1cb31be26209e
Philippe Teuwen <phil@teuwen.org>
2020-04-18 13:56:26 +0200

    cmake: fix zlib defines, keep them local

--- client/deps/zlib.cmake
+++ client/deps/zlib.cmake
@@ -1,6 +1,4 @@
 set_property(SOURCE PROPERTY C_STANDARD 99)
-add_definitions(-D_ISOC99_SOURCE -DZ_SOLO -DNO_GZIP -DZLIB_PM3_TUNED)
-include_directories(../../common/zlib)
 
 add_library(z
         ../../common/zlib/deflate.c
@@ -12,4 +10,4 @@ add_library(z
         ../../common/zlib/inftrees.c
 )
 
-
+target_compile_definitions(z PRIVATE Z_SOLO NO_GZIP ZLIB_PM3_TUNED)
