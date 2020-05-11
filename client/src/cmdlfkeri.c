30184b2b5ce3a6742c16cfa08d3b5b86a6ad25ea
iceman1001 <iceman@iuse.se>
2020-05-11 22:34:54 +0200

    cleaning

--- client/src/cmdlfkeri.c
+++ client/src/cmdlfkeri.c
@@ -269,19 +269,6 @@ static int CmdKeriClone(const char *Cmd) {
         }
     }
 
-    // this is managed in above code
-    // internalid = param_get32ex(Cmd, 0, 0, 10);
-    /*
-        // Q5 is caught in the while loop
-        //Q5
-        if (tolower(param_getchar(Cmd, 1)) == 'q') {
-            blocks[0] =
-                T5555_MODULATION_PSK1 |
-                T5555_SET_BITRATE(128) |
-                T5555_PSK_RF_2 |
-                2 << T5555_MAXBLOCK_SHIFT;
-        }
-    */
     // Setup card data/build internal id
     switch (keritype) {
         case 'i' : // Internal ID
