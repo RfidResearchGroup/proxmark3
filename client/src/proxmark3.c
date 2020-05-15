8c656492582bff98f0b257bb380c8e63b5f88f27
iceman1001 <iceman@iuse.se>
2020-05-15 17:04:33 +0200

    better fault text

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -241,8 +241,11 @@ check_script:
 
             // read script file
             if (fgets(script_cmd_buf, sizeof(script_cmd_buf), current_cmdscriptfile()) == NULL) {
+                PrintAndLogEx(ERR, "Unexpected end, [%s]", current_cmdscriptfile());
+
                 if (!pop_cmdscriptfile())
                     break;
+
                 goto check_script;
             } else {
                 prompt_ctx = PROXPROMPT_CTX_SCRIPTFILE;
