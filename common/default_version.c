833bc4d9a3f2c1dcef4a72393663f75e0ba0fbd1
iceman1001 <iceman@iuse.se>
2020-05-21 21:55:15 +0200

    Release v4.9237 - Ice Coffee :coffee:

--- common/default_version.c
+++ common/default_version.c
@@ -1,5 +1,5 @@
 #include "common.h"
-/* This is the default version.c file that Makefile.common falls back to if neither sh nor perl are available */
+/* Generated file, do not edit */
 #ifndef ON_DEVICE
 #define SECTVERSINFO
 #else
@@ -8,8 +8,9 @@
 
 const struct version_information SECTVERSINFO version_information = {
     VERSION_INFORMATION_MAGIC,
-    1, /* version 1 */
-    0, /* version information not present */
-    2, /* cleanliness couldn't be determined */
-    /* Remaining fields: zero */
+    1,
+    1,
+    2,
+    "RRG/Iceman/master/v4.9237",
+    "2020-05-21 21:55:15",
 };
