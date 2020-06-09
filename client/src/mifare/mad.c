17f56479ea96bf6cb53f4d394adb4fc34213ed94
iceman1001 <iceman@iuse.se>
2020-06-09 16:59:20 +0200

    fix coverity

--- client/src/mifare/mad.c
+++ client/src/mifare/mad.c
@@ -20,78 +20,6 @@
 // https://www.nxp.com/docs/en/application-note/AN10787.pdf
 static json_t* mad_known_aids = NULL;
 
-/*
-static madAID_t madKnownClusterCodes[] = {
-    {0x00, "cluster: card administration"},
-    {0x01, "cluster: miscellaneous applications"},
-    {0x02, "cluster: miscellaneous applications"},
-    {0x03, "cluster: miscellaneous applications"},
-    {0x04, "cluster: miscellaneous applications"},
-    {0x05, "cluster: miscellaneous applications"},
-    {0x06, "cluster: miscellaneous applications"},
-    {0x07, "cluster: miscellaneous applications"},
-    {0x08, "cluster: airlines"},
-    {0x09, "cluster: ferry traffic"},
-    {0x10, "cluster: railway services"},
-    {0x11, "cluster: miscellaneous applications"},
-    {0x12, "cluster: transport"},
-    {0x14, "cluster: security solutions"},
-    {0x18, "cluster: city traffic"},
-    {0x19, "cluster: Czech Railways"},
-    {0x20, "cluster: bus services"},
-    {0x21, "cluster: multi modal transit"},
-    {0x28, "cluster: taxi"},
-    {0x30, "cluster: road toll"},
-    {0x31, "cluster: generic transport"},
-    {0x38, "cluster: company services"},
-    {0x40, "cluster: city card services"},
-    {0x47, "cluster: access control & security"},
-    {0x48, "cluster: access control & security"},
-    {0x49, "cluster: VIGIK"},
-    {0x4A, "cluster: Ministry of Defence, Netherlands"},
-    {0x4B, "cluster: Bosch Telecom, Germany"},
-    {0x4C, "cluster: European Union Institutions"},
-    {0x50, "cluster: ski ticketing"},
-    {0x51, "cluster: access control & security"},
-    {0x52, "cluster: access control & security"},
-    {0x53, "cluster: access control & security"},
-    {0x54, "cluster: access control & security"},
-    {0x55, "cluster: SOAA standard for offline access standard"},
-    {0x56, "cluster: access control & security"},
-    {0x58, "cluster: academic services"},
-    {0x60, "cluster: food"},
-    {0x68, "cluster: non-food trade"},
-    {0x70, "cluster: hotel"},
-    {0x71, "cluster: loyalty"},
-    {0x75, "cluster: airport services"},
-    {0x78, "cluster: car rental"},
-    {0x79, "cluster: Dutch government"},
-    {0x80, "cluster: administration services"},
-    {0x88, "cluster: electronic purse"},
-    {0x90, "cluster: television"},
-    {0x91, "cluster: cruise ship"},
-    {0x95, "cluster: IOPTA"},
-    {0x97, "cluster: metering"},
-    {0x98, "cluster: telephone"},
-    {0xA0, "cluster: health services"},
-    {0xA8, "cluster: warehouse"},
-    {0xB0, "cluster: electronic trade"},
-    {0xB8, "cluster: banking"},
-    {0xC0, "cluster: entertainment & sports"},
-    {0xC8, "cluster: car parking"},
-    {0xC9, "cluster: fleet management"},
-    {0xD0, "cluster: fuel, gasoline"},
-    {0xD8, "cluster: info services"},
-    {0xE0, "cluster: press"},
-    {0xE1, "cluster: NFC Forum"},
-    {0xE8, "cluster: computer"},
-    {0xF0, "cluster: mail"},
-    {0xF8, "cluster: miscellaneous applications"},
-};
-
-*/
-
-
 static int open_mad_file(json_t **root, bool verbose) {
 
     char *path;
@@ -127,7 +55,6 @@ static int close_mad_file(json_t *root) {
     json_decref(root);
     return PM3_SUCCESS;
 }
-
 
 static const char *mad_json_get_str(json_t *data, const char *name) {
 
@@ -297,7 +224,7 @@ int MADCheck(uint8_t *sector0, uint8_t *sector10, bool verbose, bool *haveMAD2) 
             res = res2;
 
         if (verbose && !res2)
-            PrintAndLogEx(SUCCESS, "CRC8-MAD2 (%)", _GREEN_("ok"));
+            PrintAndLogEx(SUCCESS, "CRC8-MAD2 (%s)", _GREEN_("ok"));
     }
 
     return res;
