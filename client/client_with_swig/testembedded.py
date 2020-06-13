21841cb9d9bb8692e3cdd21f33a280cd1c525b9c
Philippe Teuwen <phil@teuwen.org>
2020-06-13 23:47:41 +0200

    simplify scripts

--- client/client_with_swig/testembedded.py
+++ client/client_with_swig/testembedded.py
@@ -1,12 +1,3 @@
-import sys
 import pm3
-
-def main():
-    print('Hello world')
-    #p=pm3.open("/dev/ttyACM0")
-    p=pm3.get_current_dev()
-    pm3.console(p, "hw status")
-    #pm3.close(p)
-
-if __name__ == "__main__":
-    main()
+p=pm3.get_current_dev()
+pm3.console(p, "hw status")
--- client/lib/example_lua/test.lua
+++ client/lib/example_lua/test.lua
@@ -1,3 +1,5 @@
+#!/usr/bin/env lua
+
 local pm3 = require("pm3")
 p=pm3.open("/dev/ttyACM0")
 pm3.console(p, "hw status")
