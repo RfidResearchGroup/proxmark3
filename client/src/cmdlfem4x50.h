2b1bfc58f9f05dc77c7a070423b8d5297e9c019d
tharexde <joerg.classen@bowsky.de>
2020-06-15 14:31:43 +0200

    added EM4x50 info function

---
+++ client/src/cmdlfem4x50.h
@@ -0,0 +1,18 @@
+//-----------------------------------------------------------------------------
+// Copyright (C) 2020 tharexde
+//
+// This code is licensed to you under the terms of the GNU GPL, version 2 or,
+// at your option, any later version. See the LICENSE.txt file for the text of
+// the license.
+//-----------------------------------------------------------------------------
+// Low frequency EM4x50 commands
+//-----------------------------------------------------------------------------
+
+#ifndef CMDLFEM4X50_H__
+#define CMDLFEM4X50_H__
+
+int usage_lf_em4x50_info(void);
+
+int CmdEM4x50Info(const char *Cmd);
+
+#endif
