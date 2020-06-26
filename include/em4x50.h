972ad78a40b9ba9203ed90dab31fa4f3e104f99f
tharexde <joerg.classen@bowsky.de>
2020-06-26 14:21:54 +0200

    rearranged defines

--- include/em4x50.h
+++ include/em4x50.h
@@ -11,9 +11,36 @@
 #ifndef EM4X50_H__
 #define EM4X50_H__
 
+#define EM4X50_NO_WORDS             34
+
+// special words
+#define EM4X50_DEVICE_PASSWORD      0
+#define EM4X50_PROTECTION           1
+#define EM4X50_CONTROL              2
+#define EM4X50_DEVICE_SERIAL        32
+#define EM4X50_DEVICE_ID            33
+
+// control word (word = 4 bytes)
+#define FIRST_WORD_READ             0       // first byte
+#define LAST_WORD_READ              1       // second byte
+#define CONFIG_BLOCK                2       // third byte
+#define PASSWORD_CHECK              0x80    // first bit in third byte
+#define READ_AFTER_WRITE            0x40    // second bit in third byte
+
+// protection word
+#define FIRST_WORD_READ_PROTECTED   0       // first byte
+#define LAST_WORD_READ_PROTECTED    1       // second byte
+#define FIRST_WORD_WRITE_INHIBITED  2       // third byte
+#define LAST_WORD_WRITE_INHIBITED   3       // fourth byte
+
+// misc
+#define STATUS_NO_WORDS             0xfc
+#define STATUS_SUCCESS              0x2
+#define STATUS_LOGIN                0x1
+#define NO_CHARS_MAX                400
+
 typedef struct {
-    bool fwr_given;
-    bool lwr_given;
+    bool addr_given;
     bool pwd_given;
     bool newpwd_given;
     uint8_t password[4];
