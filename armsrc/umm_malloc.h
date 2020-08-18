2f31990346843308b2281e0668323a47e6195cf1
Philippe Teuwen <phil@teuwen.org>
2020-08-18 21:59:56 +0200

    upstream umm_malloc

---
+++ armsrc/umm_malloc.h
@@ -0,0 +1,31 @@
+/* ----------------------------------------------------------------------------
+ * umm_malloc.h - a memory allocator for embedded systems (microcontrollers)
+ *
+ * See copyright notice in LICENSE.TXT
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef UMM_MALLOC_H
+#define UMM_MALLOC_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ------------------------------------------------------------------------ */
+
+extern void  umm_init( void );
+extern void *umm_malloc( size_t size );
+extern void *umm_calloc( size_t num, size_t size );
+extern void *umm_realloc( void *ptr, size_t size );
+extern void  umm_free( void *ptr );
+
+/* ------------------------------------------------------------------------ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UMM_MALLOC_H */
