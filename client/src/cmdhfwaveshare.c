9209f8fb7f305d79afa943619ae343c1c76f0176
Philippe Teuwen <phil@teuwen.org>
2020-09-15 23:00:13 +0200

    ws: another guess at width/height based on Android SDK decompilation

--- client/src/cmdhfwaveshare.c
+++ client/src/cmdhfwaveshare.c
@@ -79,9 +79,9 @@ typedef enum {
 
 static model_t models[] = {
     {"2.13 inch e-paper",    16, 122, 250}, // tested
-    {"2.9 inch e-paper",     16, 128, 296},
+    {"2.9 inch e-paper",     16, 296, 128},
     {"4.2 inch e-paper",    100, 400, 300}, // tested
-    {"7.5 inch e-paper",    120, 480, 800},
+    {"7.5 inch e-paper",    120, 800, 480},
     {"2.7 inch e-paper",    121, 276, 176},
     {"2.13 inch e-paper B", 106, 212, 104},
     {"1.54 inch e-paper B", 100, 200, 200},
