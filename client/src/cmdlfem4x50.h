688a2e741cda1c9ee7d3135465147057b75764a5
tharexde <joerg.classen@bowsky.de>
2020-09-27 12:56:18 +0200

    new entry for function 4x50_bruteforce

--- client/src/cmdlfem4x50.h
+++ client/src/cmdlfem4x50.h
@@ -24,5 +24,6 @@ int CmdEM4x50WritePassword(const char *Cmd);
 int CmdEM4x50Read(const char *Cmd);
 int CmdEM4x50Dump(const char *Cmd);
 int CmdEM4x50Wipe(const char *Cmd);
+int CmdEM4x50BruteForce(const char *Cmd);
 
 #endif
