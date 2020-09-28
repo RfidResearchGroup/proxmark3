3a5c7430b5cd0983a3ecda43159e9c31a1711f82
Msprg <18015488+Msprg@users.noreply.github.com>
2020-09-28 18:11:40 +0200

    Edited "protect" help text for more accuracy
    Yeah, I got nothing better to do, than just push fixes from discord.
    
    Anyways, I think this should work...

--- client/src/cmdlft55xx.c
+++ client/src/cmdlft55xx.c
@@ -355,7 +355,7 @@ static int usage_t55xx_protect(void) {
     PrintAndLogEx(NORMAL, "");
     PrintAndLogEx(NORMAL, "Examples:");
     PrintAndLogEx(NORMAL, _YELLOW_("      lf t55xx protect n 01020304") "         - sets new password to 01020304");
-    PrintAndLogEx(NORMAL, _YELLOW_("      lf t55xx protect p 11223344") "         - use pwd 11223344 to set newpwd to 00000000");
+    PrintAndLogEx(NORMAL, _YELLOW_("      lf t55xx protect p 11223344 n 00000000") "         - use pwd 11223344 to set newpwd to 00000000");
     PrintAndLogEx(NORMAL, "");
     return PM3_SUCCESS;
 }
