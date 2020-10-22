452ad19bb939e27a5bb76f6ed8ae540998b964fb
iceman1001 <iceman@iuse.se>
2020-10-22 17:44:37 +0200

    fix https://github.com/RfidResearchGroup/proxmark3/issues/1028

--- client/src/cmdlfhid.c
+++ client/src/cmdlfhid.c
@@ -433,8 +433,6 @@ static int CmdHIDBrute(const char *Cmd) {
     cn_hi.OEM = arg_get_int_def(ctx, 6, 0);
     delay = arg_get_int_def(ctx, 7, 1000);
 
-    CLIParserFree(ctx);
-
     if (arg_get_lit(ctx, 8) && arg_get_lit(ctx, 9)) {
         direction = 0;
     } else if (arg_get_lit(ctx, 8)) {
@@ -443,6 +441,8 @@ static int CmdHIDBrute(const char *Cmd) {
         direction = 2;
     }
 
+    CLIParserFree(ctx);
+    
     if (verbose) {
         PrintAndLogEx(INFO, "Wiegand format#.. %i", format_idx);
         PrintAndLogEx(INFO, "OEM#............. %u", cn_hi.OEM);
