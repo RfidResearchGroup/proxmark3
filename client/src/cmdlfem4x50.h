881bdbf456dd977c2c2f95504a541e7dc0dc7d9a
tharexde <joerg.classen@bowsky.de>
2020-10-25 16:39:22 +0100

    added new function 4x50_restore

--- client/src/cmdlfem4x50.h
+++ client/src/cmdlfem4x50.h
@@ -28,5 +28,6 @@ int CmdEM4x50Brute(const char *Cmd);
 int CmdEM4x50Login(const char *Cmd);
 int CmdEM4x50Reset(const char *Cmd);
 int CmdEM4x50Watch(const char *Cmd);
+int CmdEM4x50Restore(const char *Cmd);
 
 #endif
