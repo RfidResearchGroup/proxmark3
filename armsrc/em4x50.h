f412b20fc1b58c7e42659ad49a08bdd664cb6d2a
tharexde <joerg.classen@bowsky.de>
2020-10-31 01:50:03 +0100

    sing emulator memory for sim and restore

--- armsrc/em4x50.h
+++ armsrc/em4x50.h
@@ -29,7 +29,7 @@ void em4x50_login(uint32_t *password);
 void em4x50_reset(void);
 void em4x50_watch(void);
 void em4x50_restore(em4x50_data_t *etd);
-void em4x50_sim(em4x50_data_t *etd);
+void em4x50_sim(void);
 void em4x50_std_read(void);
 
 #endif /* EM4X50_H */
