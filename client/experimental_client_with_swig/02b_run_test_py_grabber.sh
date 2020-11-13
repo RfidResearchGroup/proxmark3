923d2942c237721802f36dd0a0822cda7fe02237
Philippe Teuwen <phil@teuwen.org>
2020-11-14 00:44:18 +0100

    py scripts

--- client/experimental_client_with_swig/02b_run_test_py_grabber.sh
+++ client/experimental_client_with_swig/02b_run_test_py_grabber.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#/usr/local/lib/python3/dist-packages/pm3.py
-#/usr/lib/python3/dist-packages/pm3.py
-
-# need access to pm3.py
-PYTHONPATH=../src ../../pm3 -c "script run testembedded_grab.py" -i
+../../pm3 -c "script run testembedded_grab.py" -i
