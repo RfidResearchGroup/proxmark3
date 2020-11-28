ae04b07c13cd220f78f4a4acc98ff1b36c859b64
iceman1001 <iceman@iuse.se>
2020-11-28 22:19:25 +0100

    optional

--- client/src/cmdlfsecurakey.c
+++ client/src/cmdlfsecurakey.c
@@ -154,7 +154,7 @@ static int CmdSecurakeyClone(const char *Cmd) {
 
     void *argtable[] = {
         arg_param_begin,
-        arg_str0("r", "raw", "<hex>", "raw hex data. 12 bytes"),
+        arg_str1("r", "raw", "<hex>", "raw hex data. 12 bytes"),
         arg_lit0(NULL, "q5", "optional - specify writing to Q5/T5555 tag"),
         arg_lit0(NULL, "em", "optional - specify writing to EM4305/4469 tag"),
         arg_param_end
