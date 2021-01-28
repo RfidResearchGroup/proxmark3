2c73337bde37e2271534b621d454c85e2fe6e364
iceman1001 <iceman@iuse.se>
2021-01-28 14:32:57 +0100

    cppchecker ,, silence a warning

--- client/src/preferences.c
+++ client/src/preferences.c
@@ -834,7 +834,7 @@ static int setCmdSavePaths(const char *Cmd) {
     CLIParserFree(ctx);
 
     if (deflen == 0 && dulen == 0 && tlen == 0) {
-        PrintAndLogEx(FAILED, "Must give atleast one path");
+        PrintAndLogEx(FAILED, "Must give at least one path");
         return PM3_EINVARG;
     }
 
@@ -851,6 +851,11 @@ static int setCmdSavePaths(const char *Cmd) {
     if (tlen) {
         path_item = spTrace;
         path = trace_path;
+    }
+
+    if (path == NULL) {
+        PrintAndLogEx(FAILED, "Must give at least one path");
+        return PM3_EINVARG;
     }
 
     // remove trailing slash.
