62031e640cadbccc2567eeb9770bc297f78f4fff
iceman1001 <iceman@iuse.se>
2021-03-30 09:34:17 +0200

    swap arrays

--- client/src/mifare/desfire_crypto.c
+++ client/src/mifare/desfire_crypto.c
@@ -870,7 +870,7 @@ void mifare_cypher_single_block(desfirekey_t key, uint8_t *data, uint8_t *ivect,
                     mbedtls_aes_context actx;
                     mbedtls_aes_init(&actx);
                     mbedtls_aes_setkey_dec(&actx, key->data, 128);
-                    mbedtls_aes_crypt_ecb(&actx, MBEDTLS_AES_DECRYPT, edata, data);
+                    mbedtls_aes_crypt_ecb(&actx, MBEDTLS_AES_DECRYPT, data, edata);
                     mbedtls_aes_free(&actx);
                     break;
                 }
