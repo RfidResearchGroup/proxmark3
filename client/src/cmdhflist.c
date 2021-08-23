8edcfb925beb45bbe1e5d0ae29eb5afe8c324246
iceman1001 <iceman@iuse.se>
2021-08-23 07:25:25 +0200

    text

--- client/src/cmdhflist.c
+++ client/src/cmdhflist.c
@@ -345,9 +345,11 @@ int applyIso14443a(char *exp, size_t size, uint8_t *cmd, uint8_t cmdsize) {
             break;
         }
         case NTAG_I2C_FASTWRITE: 
-            if (cmdsize == 69) {
+            if (cmdsize == 69)
                 snprintf(exp, size, "FAST WRITE (%d - %d)", cmd[1], cmd[2]);
-            }
+            else
+                snprintf(exp, size, "?");
+            
             break;
         default:
             return 0;
