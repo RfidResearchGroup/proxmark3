a11648717cae5cb6680930cd3e1c78067eee1a70
mwalker33 <51802811+mwalker33@users.noreply.github.com>
2021-10-14 15:13:37 +1100

    Update appmain.c

--- armsrc/appmain.c
+++ armsrc/appmain.c
@@ -163,9 +163,7 @@ static void MeasureAntennaTuning(void) {
         uint8_t results[256];
     } PACKED payload;
 
-    // Need to clear all values to ensure non-random responses.
-    memset(&payload, 0, sizeof(payload));
-    // memset(payload.results, 0, sizeof(payload.results));
+    memset(payload.results, 0, sizeof(payload.results));
 
     sample_config *sc = getSamplingConfig();
     payload.divisor = sc->divisor;
