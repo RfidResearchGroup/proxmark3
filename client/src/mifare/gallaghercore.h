f0b82c63116bf69f1b0bb0acc1ea764337ff1476
Matt Moran <mattmoran2000@gmail.com>
2021-12-29 20:54:03 +1300

    Extract gallagher encoding/decoding
    Change input parameters to be `arg_u640` so that we can check they are the right lengths (and not have them silently truncated)

---
+++ client/src/mifare/gallaghercore.h
@@ -0,0 +1,22 @@
+/**
+ * Matt Moran (@DarkMatterMatt), 2021
+ * -----------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * -----------------------------------------------------------------------------
+ * Common functionality for low/high-frequency GALLAGHER tag encoding & decoding.
+ */
+#ifndef MIFARE_GALLAGHERCORE_H__
+#define MIFARE_GALLAGHERCORE_H__
+
+#include "common.h"
+
+void encodeCardholderCredentials(uint8_t *eight_bytes, uint8_t region_code, uint16_t facility_code, uint32_t card_number, uint8_t issue_level);
+
+void decodeCardholderCredentials(uint8_t *eight_bytes, uint8_t *region_code, uint16_t *facility_code, uint32_t *card_number, uint8_t *issue_level);
+
+bool isValidGallagherCredentials(uint64_t region_code, uint64_t facility_code, uint64_t card_number, uint64_t issue_level);
+
+#endif
