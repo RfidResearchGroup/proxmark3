0cb1cfee6b399ee810bbfc6b615b9679342e2bbc
iceman1001 <iceman@iuse.se>
2021-12-30 22:06:43 +0100

    text

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -38,7 +38,7 @@
 
 #ifndef LIBPM3
 #define BANNERMSG1 ""
-#define BANNERMSG2 "   [ :snowflake: Iceman :snowflake: ]"
+#define BANNERMSG2 "   [ Iceman :snowflake: ]"
 #define BANNERMSG3 ""
 
 typedef enum LogoMode { UTF8, ANSI, ASCII } LogoMode;
