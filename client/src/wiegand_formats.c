4919d14c13d9d07fbca55a40ca034350bfa82c38
iceman1001 <iceman@iuse.se>
2022-01-01 16:29:51 +0100

    cppcheck fix

--- client/src/wiegand_formats.c
+++ client/src/wiegand_formats.c
@@ -1414,7 +1414,7 @@ int HIDFindCardFormat(const char *format) {
 bool HIDPack(int format_idx, wiegand_card_t *card, wiegand_message_t *packed, bool preamble) {
     memset(packed, 0, sizeof(wiegand_message_t));
 
-    if (format_idx < 0 || format_idx >= ARRAYLEN(FormatTable))
+    if (format_idx < 0 || format_idx >= ARRAYLEN(FormatTable) - 1)
         return false;
 
     return FormatTable[format_idx].Pack(card, packed, preamble);
