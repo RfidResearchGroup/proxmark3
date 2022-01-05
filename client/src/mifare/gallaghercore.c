caebfdca622670e0558dad2cb28bec093dfa6c94
Matt Moran <mattmoran2000@gmail.com>
2022-01-05 23:54:18 +1300

    Update license from LGPL to GPL

--- client/src/mifare/gallaghercore.c
+++ client/src/mifare/gallaghercore.c
@@ -2,7 +2,7 @@
  * Matt Moran (@DarkMatterMatt), 2021
  * -----------------------------------------------------------------------------
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
+ * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  * -----------------------------------------------------------------------------
