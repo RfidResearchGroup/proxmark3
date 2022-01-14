327231b060585bc421bca56118cc04212bba9938
Philippe Teuwen <phil@teuwen.org>
2022-01-14 21:22:26 +0100

    license

--- client/emojis_scrap_github.py
+++ client/emojis_scrap_github.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-# Mostly derived from https://github.com/mrowa44/emojify Copyright (c) 2015 Justyna Rachowicz
+#-----------------------------------------------------------------------------
+# Borrowed initially from https://github.com/mrowa44/emojify
+# Copyright (c) 2015 Justyna Rachowicz
+# Copyright (C) Proxmark3 contributors. See AUTHORS.md for details.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See LICENSE.txt for the text of the license.
+#-----------------------------------------------------------------------------
 
 from urllib.request import urlopen
 import json
