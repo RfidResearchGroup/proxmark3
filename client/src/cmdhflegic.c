a8175a0f573a56bcd24859e76287756d51ffb74c
iceman1001 <iceman@iuse.se>
2022-02-15 20:10:39 +0100

    coverity #378901, #378902 fixes

--- client/src/cmdhflegic.c
+++ client/src/cmdhflegic.c
@@ -656,6 +656,7 @@ int legic_read_mem(uint32_t offset, uint32_t len, uint32_t iv, uint8_t *out, uin
 
     clearCommandBuffer();
     SendCommandNG(CMD_HF_LEGIC_READER, (uint8_t *)payload, sizeof(legic_packet_t));
+    free(payload);
     PacketResponseNG resp;
 
     uint8_t timeout = 0;
@@ -668,7 +669,6 @@ int legic_read_mem(uint32_t offset, uint32_t len, uint32_t iv, uint8_t *out, uin
         }
     }
     PrintAndLogEx(NORMAL, "");
-
 
     *outlen = resp.data.asDwords[0];
     if (resp.status != PM3_SUCCESS) {
@@ -819,6 +819,7 @@ static int CmdLegicDump(const char *Cmd) {
 
     clearCommandBuffer();
     SendCommandNG(CMD_HF_LEGIC_READER, (uint8_t *)payload, sizeof(legic_packet_t));
+    free(payload);
     PacketResponseNG resp;
 
     uint8_t timeout = 0;
