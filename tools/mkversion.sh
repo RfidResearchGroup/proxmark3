738963306ec9858be8d47bdb7415e52ee8787b5c
Philippe Teuwen <phil@teuwen.org>
2022-02-27 12:40:10 +0100

    mkversion: Change grep by cut because of weird grep behavior on OSX

--- tools/mkversion.sh
+++ tools/mkversion.sh
@@ -63,7 +63,7 @@ sha=$(
     cd "$pm3path" || return
     # did we find the src?
     [ -f armsrc/appmain.c ] || return
-    ls armsrc/*.[ch] common_arm/*.[ch]|grep -E -v "(disabled|version_pm3|fpga_version_info)"|xargs sha256sum|sha256sum|grep -o '^.........'
+    ls armsrc/*.[ch] common_arm/*.[ch]|grep -E -v "(disabled|version_pm3|fpga_version_info)"|xargs sha256sum|sha256sum|cut -c -9
 )
 cat <<EOF
 #include "common.h"
