320be614fec1e68b9b50a9efaacc55d80e272132
mwalker33 <51802811+mwalker33@users.noreply.github.com>
2022-05-08 15:54:56 +1000

    Update cmdhfmfdes.c
    Update format type to match supplied data type (dfnamelen)

--- client/src/cmdhfmfdes.c
+++ client/src/cmdhfmfdes.c
@@ -2547,7 +2547,7 @@ static int CmdHF14ADesCreateApp(const char *Cmd) {
         PrintAndLogEx(INFO, "ISO file ID  %s", (data[4] & 0x20) ? "enabled" : "disabled");
         if ((data[4] & 0x20)) {
             PrintAndLogEx(INFO, "ISO file ID  0x%04x", MemLeToUint2byte(&data[5]));
-            PrintAndLogEx(INFO, "DF Name[%02zu]  %s | %s\n", dfnamelen, sprint_ascii(dfname, dfnamelen), sprint_hex (dfname, dfnamelen));
+            PrintAndLogEx(INFO, "DF Name[%02d]  %s | %s\n", dfnamelen, sprint_ascii(dfname, dfnamelen), sprint_hex (dfname, dfnamelen));
         }
         PrintKeySettings(data[3], data[4], true, true);
         PrintAndLogEx(INFO, "---------------------------");
