009b91e096ef53c1c5ced5572c5efa6a3fbcb13c
iceman1001 <iceman@iuse.se>
2022-07-04 19:23:44 +0200

    limit hex print outs

--- client/src/util.c
+++ client/src/util.c
@@ -318,7 +318,7 @@ char *sprint_hex(const uint8_t *data, const size_t len) {
 }
 
 char *sprint_hex_inrow_ex(const uint8_t *data, const size_t len, const size_t min_str_len) {
-    static char buf[UTIL_BUFFER_SIZE_SPRINT] = {0};
+    static char buf[UTIL_BUFFER_SIZE_SPRINT - 3] = {0};
     hex_to_buffer((uint8_t *)buf, data, len, sizeof(buf) - 1, min_str_len, 0, true);
     return buf;
 }
@@ -327,7 +327,7 @@ char *sprint_hex_inrow(const uint8_t *data, const size_t len) {
     return sprint_hex_inrow_ex(data, len, 0);
 }
 char *sprint_hex_inrow_spaces(const uint8_t *data, const size_t len, size_t spaces_between) {
-    static char buf[UTIL_BUFFER_SIZE_SPRINT] = {0};
+    static char buf[UTIL_BUFFER_SIZE_SPRINT - 3] = {0};
     hex_to_buffer((uint8_t *)buf, data, len, sizeof(buf) - 1, 0, spaces_between, true);
     return buf;
 }
