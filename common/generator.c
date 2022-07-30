712e46ab5a841ffcfd69700ab740d34033c694de
iceman1001 <iceman@iuse.se>
2022-07-30 11:18:11 +0200

    fix #1736

--- common/generator.c
+++ common/generator.c
@@ -485,9 +485,7 @@ uint32_t lf_t55xx_white_pwdgen(uint32_t id) {
 // Gallagher Desfire Key Diversification Input for Cardax Card Data Application
 int mfdes_kdf_input_gallagher(uint8_t *uid, uint8_t uidLen, uint8_t keyNo, uint32_t aid, uint8_t *kdfInputOut, uint8_t *kdfInputLen) {
     if (uid == NULL || (uidLen != 4 && uidLen != 7) || keyNo > 2 || kdfInputOut == NULL || kdfInputLen == NULL) {
-        if (g_debugMode) {
-            prnt("Invalid arguments");
-        }
+        prnt("Invalid arguments");
         return PM3_EINVARG;
     }
 
