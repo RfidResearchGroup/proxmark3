79cfa1d8faa56c390a045d779b997633b357c515
iceman1001 <iceman@iuse.se>
2022-08-28 12:21:21 +0200

    adapt SPIFFS from 128kb -> 192kb.  Increased GC to fit sector size 4kb. (thanks @mwalker33)

--- armsrc/spiffs.h
+++ armsrc/spiffs.h
@@ -24,7 +24,11 @@ extern "C" {
 
 #include "spiffs_config.h"
 
-typedef enum spiffs_safety_level { RDV40_SPIFFS_SAFETY_NORMAL, RDV40_SPIFFS_SAFETY_LAZY, RDV40_SPIFFS_SAFETY_SAFE } RDV40SpiFFSSafetyLevel;
+typedef enum spiffs_safety_level {
+    RDV40_SPIFFS_SAFETY_NORMAL, 
+    RDV40_SPIFFS_SAFETY_LAZY, 
+    RDV40_SPIFFS_SAFETY_SAFE
+} RDV40SpiFFSSafetyLevel;
 
 typedef enum spiffs_file_type {
     RDV40_SPIFFS_FILETYPE_REAL,
@@ -198,16 +202,16 @@ typedef void (*spiffs_file_callback)(struct spiffs_t *fs, spiffs_fileop_type op,
 
 #ifndef SPIFFS_DBG
 #define SPIFFS_DBG(...) \
-    printf(__VA_ARGS__)
+    Dbprintf(__VA_ARGS__)
 #endif
 #ifndef SPIFFS_GC_DBG
-#define SPIFFS_GC_DBG(...) printf(__VA_ARGS__)
+#define SPIFFS_GC_DBG(...) Dbprintf(__VA_ARGS__)
 #endif
 #ifndef SPIFFS_CACHE_DBG
-#define SPIFFS_CACHE_DBG(...) printf(__VA_ARGS__)
+#define SPIFFS_CACHE_DBG(...) Dbprintf(__VA_ARGS__)
 #endif
 #ifndef SPIFFS_CHECK_DBG
-#define SPIFFS_CHECK_DBG(...) printf(__VA_ARGS__)
+#define SPIFFS_CHECK_DBG(...) Dbprintf(__VA_ARGS__)
 #endif
 
 /* Any write to the filehandle is appended to end of the file */
