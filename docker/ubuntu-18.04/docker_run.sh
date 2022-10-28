5245c6801debafb36f717458c32560f51919c1d0
iceman1001 <iceman@iuse.se>
2022-10-29 00:04:53 +0200

    adapt ubuntu 18 docker

--- docker/ubuntu-18.04/docker_run.sh
+++ docker/ubuntu-18.04/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --volume=$(pwd)/../..:/home/rrg/proxmark3 -it pm3-ubuntu-18.04:1.0
+docker run --volume=$(pwd)/../..:/home/rrg/proxmark3 -w /home/rrg/proxmark3 -it pm3-ubuntu-18.04:1.0
