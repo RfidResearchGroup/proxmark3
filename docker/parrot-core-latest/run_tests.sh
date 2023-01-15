cb8f1a51adc21f18bf09291a24fbf2abc48ee6cb
Philippe Teuwen <phil@teuwen.org>
2023-01-15 19:54:56 +0100

    update parrot docker tests

--- docker/parrot-core-latest/run_tests.sh
+++ docker/parrot-core-latest/run_tests.sh
@@ -2,7 +2,7 @@
 # Iceman 2022
 #
 # This script is to be run from proxmark root folder inside the docker env
-# docker/ubuntu-18.04/run_tests.sh;
+# docker/parrot-core-latest/run_tests.sh;
 
 sudo apt update && sudo apt upgrade -y
 tools/release_tests.sh
--- docker/ubuntu-20.04/run_tests.sh
+++ docker/ubuntu-20.04/run_tests.sh
@@ -2,7 +2,7 @@
 # Iceman 2022
 #
 # This script is to be run from proxmark root folder inside the docker env
-# docker/debian-bullseye/run_tests.sh;
+# docker/ubuntu-20.04/run_tests.sh;
 
 sudo apt update && sudo apt upgrade -y
 tools/release_tests.sh
