bec61e9185778afdb08cd4334cc4272f1425a74f
iceman1001 <iceman@iuse.se>
2023-01-29 20:35:17 +0100

    Release v4.16191 - Nitride

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -42,7 +42,7 @@ static int mainret = PM3_ESOFT;
 #ifndef LIBPM3
 #define BANNERMSG1 ""
 #define BANNERMSG2 "   [ :coffee: ]"
-#define BANNERMSG3 ""
+#define BANNERMSG3 "Release v4.16191 - Nitride"
 
 typedef enum LogoMode { UTF8, ANSI, ASCII } LogoMode;
 
