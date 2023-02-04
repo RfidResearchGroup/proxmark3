45bff8ec05e34924974864706c0ee8c5538d565f
Henry Gabryjelski <henrygab@users.noreply.github.com>
2023-02-04 13:19:56 -0800

    fix inverted logic for success/failure

--- armsrc/em4x70.c
+++ armsrc/em4x70.c
@@ -876,8 +876,8 @@ void em4x70_NEW_COMMAND_XYZZY(em4x70_data_t *etd, bool ledcontrol) {
 
     StopTicks();
     lf_finalize(ledcontrol);
-    Dbprintf("*** " __FILE__ " @ %4d    final call to reply_ng.", __LINE__);
-    reply_ng(CMD_LF_EM4X70_NEW_COMMAND_XYZZY, PM3_SUCCESS, results, 4);
+    Dbprintf("*** " __FILE__ " @ %4d    final call to reply_ng.  (compiled at %s)", __LINE__, __TIME__);
+    reply_ng(CMD_LF_EM4X70_NEW_COMMAND_XYZZY, 1, results, 4);
 }
 
 
