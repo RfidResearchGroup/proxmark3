a54097e19cddf6ceee9dc9deccacb02d432cfd1e
Philippe Teuwen <phil@teuwen.org>
2023-07-16 17:25:43 +0200

    fix archlinux and fedora dockers

--- docker/archlinux/run_tests.sh
+++ docker/archlinux/run_tests.sh
@@ -39,4 +39,9 @@ pacman -Ss '^gcc$'
 # sudo pacman -S testing/gcc
 # sudo pacman -S gcc
 
+python3 -m venv /tmp/venv
+source /tmp/venv/bin/activate
+python3 -m pip install --use-pep517 pyaes
+python3 -m pip install ansicolors sslcrypto
 tools/release_tests.sh
+deactivate
--- docker/fedora-36/docker_rm.sh
+++ docker/fedora-36/docker_rm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker rm $(docker ps -aq --filter ancestor=pm3-fedora-36:1.0)
 docker image rm pm3-fedora-36:1.0
 docker image rm fedora:36
