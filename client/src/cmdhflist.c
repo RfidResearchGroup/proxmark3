RfidResearchGroup/proxmark3

commit 8543db11b59f34ca120a1e6eea7f5b43adee8dac

Avoid useless loop by moving check to the default case

client/src/cmdhflist.c
@@ -1264,13 +1264,6 @@ void annotateMfPlus(char *exp, size_t size, uint8_t *cmd, uint8_t cmdsize) {
                 data_size = cmdsize - (data - cmd);
             }
 
-            // Messages for commands that do not need args are treated first
-            const char *annotation = mfpGetAnnotationForCode(cmd[pos]) ;
-            if (annotation != NULL) {
-                snprintf(exp, size, "%s", annotation) ;
-                break ;
-            }
-
             switch (cmd[pos]) {
                 case MFP_AUTHENTICATEFIRST:
                 case MFP_AUTHENTICATEFIRST_VARIANT:
@@ -1407,7 +1400,13 @@ void annotateMfPlus(char *exp, size_t size, uint8_t *cmd, uint8_t cmdsize) {
                     break;
 
                 default:
-                    found_annotation = false;
+                    // Messages for commands that do not need args are treated here
+                    const char *annotation = mfpGetAnnotationForCode(cmd[pos]) ;
+                    if (annotation != NULL) {
+                        snprintf(exp, size, "%s", annotation) ;
+                    } else {
+                        found_annotation = false;
+                    }
                     break;
             }
             if (found_annotation) {

commit 89cbe3c7df34f7a98828c56d2f6d1dea2a1d5d07

should fix macos compilation error introduced by 8543db11b59f34ca120a1e6eea7f5b43adee8dac

client/src/cmdhflist.c
@@ -1399,7 +1399,7 @@ void annotateMfPlus(char *exp, size_t size, uint8_t *cmd, uint8_t cmdsize) {
                     }
                     break;
 
-                default:
+                default: {
                     // Messages for commands that do not need args are treated here
                     const char *annotation = mfpGetAnnotationForCode(cmd[pos]) ;
                     if (annotation != NULL) {
@@ -1408,6 +1408,7 @@ void annotateMfPlus(char *exp, size_t size, uint8_t *cmd, uint8_t cmdsize) {
                         found_annotation = false;
                     }
                     break;
+                }
             }
             if (found_annotation) {
                 break;