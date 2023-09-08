7b5c35008903b5afa0faff74cc77724f5499f8fc
Philippe Teuwen <phil@teuwen.org>
2023-09-08 15:46:49 +0200

    fix Tumbleweed docker

--- docker/opensuse-tumbleweed/run_tests.sh
+++ docker/opensuse-tumbleweed/run_tests.sh
@@ -5,4 +5,9 @@
 # docker/opensuse-tumbleweed/run_tests.sh;
 
 sudo zypper refresh && sudo zypper --non-interactive update
+python3 -m venv /tmp/venv
+source /tmp/venv/bin/activate
+python3 -m pip install --use-pep517 pyaes
+python3 -m pip install ansicolors sslcrypto
 tools/release_tests.sh
+deactivate
