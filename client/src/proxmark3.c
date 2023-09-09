6ca0ce704afc400a9682f0ce7daff24c6857252c
iceman1001 <iceman@iuse.se>
2023-09-09 11:00:00 +0200

    Release v4.17140 - Raccoon

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -42,7 +42,7 @@ static int mainret = PM3_ESOFT;
 #ifndef LIBPM3
 #define BANNERMSG1 ""
 #define BANNERMSG2 "   [ :coffee: ]"
-#define BANNERMSG3 ""
+#define BANNERMSG3 "Release v4.17140 - Raccoon"
 
 typedef enum LogoMode { UTF8, ANSI, ASCII } LogoMode;
 
