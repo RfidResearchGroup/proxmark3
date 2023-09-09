f9877f79de6ebe6ef57fcb3a52d31e2858d9276f
iceman1001 <iceman@iuse.se>
2023-09-09 11:00:00 +0200

    Revert "Release v4.17140 - Raccoon"
    This reverts commit 6ca0ce704afc400a9682f0ce7daff24c6857252c.

--- common/default_version_pm3.c
+++ common/default_version_pm3.c
@@ -1,5 +1,20 @@
+//-----------------------------------------------------------------------------
+// Copyright (C) Proxmark3 contributors. See AUTHORS.md for details.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See LICENSE.txt for the text of the license.
+//-----------------------------------------------------------------------------
 #include "common.h"
-/* Generated file, do not edit */
+/* This is the default version_pm3.c file that Makefile.common falls back to if sh is not available */
 #ifndef ON_DEVICE
 #define SECTVERSINFO
 #else
@@ -8,10 +23,10 @@
 
 const struct version_information_t SECTVERSINFO g_version_information = {
     VERSION_INFORMATION_MAGIC,
-    1,
-    1,
-    2,
-    "Iceman/master/v4.17140",
-    "2023-09-09 11:00:00",
-    "5901f2664"
+    1, /* version 1 */
+    0, /* version information not present */
+    2, /* cleanliness couldn't be determined */
+    "Iceman/master/unknown",
+    "1970-01-01 00:00:00",
+    "no sha256"
 };
