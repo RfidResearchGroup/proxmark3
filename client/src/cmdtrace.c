6b8a269671fd35447dcd3c8436b73583a5613ab9
Anonymous <hazardousvoltage312@gmail.com>
2023-12-06 02:52:15 -0500

    cmdtrace.c: fix segfault when trying to list trace after loading fails, part deux

--- client/src/cmdtrace.c
+++ client/src/cmdtrace.c
@@ -1293,7 +1293,7 @@ int CmdTraceList(const char *Cmd) {
 
     if (use_buffer == false) {
         download_trace();
-    } else if (gs_traceLen == 0) {
+    } else if (gs_traceLen == 0 || gs_trace ==NULL) {
         PrintAndLogEx(FAILED, "You requested a trace list in offline mode but there is no trace.");
         PrintAndLogEx(FAILED, "Consider using " _YELLOW_("`trace load`") " or removing parameter " _YELLOW_("`-1`"));
         return PM3_EINVARG;
