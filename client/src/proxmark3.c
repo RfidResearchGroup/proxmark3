b1a1ca88278b1674128c11221079052d4161909e
iceman1001 <iceman@iuse.se>
2024-01-03 01:59:39 +0100

    Revert "Release v4.17768 - Steamboat Willie"
    This reverts commit 57eeb712fd1dba330c6ed81377b3e074a962f7a7.

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -42,7 +42,7 @@ static int mainret = PM3_ESOFT;
 #ifndef LIBPM3
 #define BANNERMSG1 ""
 #define BANNERMSG2 "   [ :coffee: ]"
-#define BANNERMSG3 "Release v4.17768 - Steamboat Willie"
+#define BANNERMSG3 ""
 
 typedef enum LogoMode { UTF8, ANSI, ASCII } LogoMode;
 
