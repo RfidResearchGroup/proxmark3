57eeb712fd1dba330c6ed81377b3e074a962f7a7
iceman1001 <iceman@iuse.se>
2024-01-03 01:59:39 +0100

    Release v4.17768 - Steamboat Willie

--- common/default_version_pm3.c
+++ common/default_version_pm3.c
@@ -1,20 +1,5 @@
-//-----------------------------------------------------------------------------
-// Copyright (C) Proxmark3 contributors. See AUTHORS.md for details.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// See LICENSE.txt for the text of the license.
-//-----------------------------------------------------------------------------
 #include "common.h"
-/* This is the default version_pm3.c file that Makefile.common falls back to if sh is not available */
+/* Generated file, do not edit */
 #ifndef ON_DEVICE
 #define SECTVERSINFO
 #else
@@ -23,10 +8,10 @@
 
 const struct version_information_t SECTVERSINFO g_version_information = {
     VERSION_INFORMATION_MAGIC,
-    1, /* version 1 */
-    0, /* version information not present */
-    2, /* cleanliness couldn't be determined */
-    "Iceman/master/unknown",
-    "1970-01-01 00:00:00",
-    "no sha256"
+    1,
+    1,
+    2,
+    "Iceman/master/v4.17768",
+    "2024-01-03 01:59:39",
+    "4aa4f5078"
 };
