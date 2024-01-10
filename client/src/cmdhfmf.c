641308e29cec2fd1429a0abc5495efd5c26a84c5
iceman1001 <iceman@iuse.se>
2024-01-10 09:24:52 +0100

    detection

--- client/src/cmdhfmf.c
+++ client/src/cmdhfmf.c
@@ -8966,6 +8966,7 @@ static int CmdHF14AMfInfo(const char *Cmd) {
         if (
             (blockdata[8] == 0x03 && blockdata[15] == 0x90) ||
             (blockdata[9] == 0x02 && blockdata[14] == 0x1D) ||
+            (blockdata[8] == 0x04 && blockdata[15] == 0x90) ||
             (memcmp(blockdata + 8, "\x62\x63\x64\x65\x66\x67\x68\x69", 8) == 0)
         ) {
             PrintAndLogEx(SUCCESS, "   Fudan tag detected");
