d5e843c7501a6f04b87fa83cf1e8c05a3d179cb2
iceman1001 <iceman@iuse.se>
2024-02-04 11:36:29 +0100

    backport - https://github.com/lua/lua/commit/dace83ea301cbeb55f4eda60c40ca2d43edec65c

--- client/deps/liblua/lobject.h
+++ client/deps/liblua/lobject.h
@@ -561,12 +561,12 @@ typedef struct Table {
     CommonHeader;
     lu_byte flags;  /* 1<<p means tagmethod(p) is not present */
     lu_byte lsizenode;  /* log2 of size of `node' array */
-    struct Table *metatable;
+    int sizearray; /* size of `array' array */
     TValue *array;  /* array part */
     Node *node;
     Node *lastfree;  /* any free position is before this position */
+    struct Table *metatable;
     GCObject *gclist;
-    int sizearray;  /* size of `array' array */
 } Table;
 
 
