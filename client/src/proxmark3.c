ac46d81244607dc0967f2dbb69cc5dec0647a740
iceman1001 <iceman@iuse.se>
2024-02-18 20:53:52 +0100

    Revert "Release v4.18218 - DEFCON is Cancelled"
    This reverts commit 8b6d8cbb79a775dae9b466fb0fb560bf7d20e4fa.

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -42,7 +42,7 @@ static int mainret = PM3_ESOFT;
 #ifndef LIBPM3
 #define BANNERMSG1 ""
 #define BANNERMSG2 "   [ :coffee: ]"
-#define BANNERMSG3 "Release v4.18218 - DEFCON is Cancelled"
+#define BANNERMSG3 ""
 
 typedef enum LogoMode { UTF8, ANSI, ASCII } LogoMode;
 
--- common/default_version_pm3.c
+++ common/default_version_pm3.c
@@ -1,5 +1,20 @@
+//-----------------------------------------------------------------------------
+// Copyright (C) Proxmark3 contributors. See AUTHORS.md for details.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See LICENSE.txt for the text of the license.
+//-----------------------------------------------------------------------------
 #include "common.h"
-/* Generated file, do not edit */
+/* This is the default version_pm3.c file that Makefile.common falls back to if sh is not available */
 #ifndef ON_DEVICE
 #define SECTVERSINFO
 #else
@@ -8,10 +23,10 @@
 
 const struct version_information_t SECTVERSINFO g_version_information = {
     VERSION_INFORMATION_MAGIC,
-    1,
-    1,
-    2,
-    "Iceman/master/v4.18218",
-    "2024-02-18 20:53:52",
-    "0c9b909e7"
+    1, /* version 1 */
+    0, /* version information not present */
+    2, /* cleanliness couldn't be determined */
+    "Iceman/master/unknown",
+    "1970-01-01 00:00:00",
+    "no sha256"
 };
