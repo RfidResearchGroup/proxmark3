a907e93d1903e6d7709130c5c71009da40115de9
Philippe Teuwen <phil@teuwen.org>
2024-09-10 01:17:25 +0200

    fix parrot dockerfile

--- docker/parrot-core-latest/run_tests.sh
+++ docker/parrot-core-latest/run_tests.sh
@@ -5,4 +5,8 @@
 # docker/parrot-core-latest/run_tests.sh;
 
 sudo apt update && sudo apt upgrade -y
+python3 -m venv /tmp/venv
+source /tmp/venv/bin/activate
+python3 -m pip install --use-pep517 pyaes
+python3 -m pip install ansicolors sslcrypto
 tools/release_tests.sh
