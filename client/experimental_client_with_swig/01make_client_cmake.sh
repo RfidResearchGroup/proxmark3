13cad28d2a3e5b1b31f2cf30d32bd685761909c7
Philippe Teuwen <phil@teuwen.org>
2024-09-10 00:09:55 +0200

    fix experimental_client_with_swig cmake tests

--- client/experimental_client_with_swig/01make_client_cmake.sh
+++ client/experimental_client_with_swig/01make_client_cmake.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cd ..
-rm -rf build
-mkdir build
 (
-  cd build
-  cmake ..
-  make -j
+  cd ..
+  rm -rf build
+  mkdir build
+  (
+    cd build
+    cmake ..
+    make -j
+  )
+  rm proxmark3
+  ln -s build/proxmark3 .
 )
+ln -s ../pyscripts/pm3.py
