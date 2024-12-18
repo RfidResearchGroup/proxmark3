a1a9b50d241f5e768c64ff9eafdb71689206fdc7
Philippe Teuwen <phil@teuwen.org>
2024-12-18 15:29:26 +0100

    Docker: fix test for pm3 when building images

--- docker/debian-12-bookworm/docker_build.sh
+++ docker/debian-12-bookworm/docker_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 . docker_conf
-UART_PORT="$(../../pm3 --list|head -n1|cut -d' ' -f2)"
-UART_GID="$(stat -c '%g' $UART_PORT)"
-docker build --build-arg UART_GID="$UART_GID" -t "$DOCKER_IMAGE" .
+# Make sure to connect a Proxmark3 when building if you want to be able to access it from within the Docker instance
+UART_PORT="$(../../pm3 --list|grep /dev|head -n1|cut -d' ' -f2)"
+if [ -n "$UART_PORT" ]; then
+    UART_GID="$(stat -c '%g' $UART_PORT)"
+    BUILDARG="--build-arg UART_GID=$UART_GID"
+else
+    BUILDARG=""
+fi
+docker build $BUILDARG -t "$DOCKER_IMAGE" .
