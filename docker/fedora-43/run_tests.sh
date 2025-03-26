0f53e9a0cb7169866b5829cd3a500174785b541d
Philippe Teuwen <phil@teuwen.org>
2025-03-26 01:10:21 +0100

    docker: add Fedora 43 (rawhide)

---
+++ docker/fedora-43/run_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Iceman 2022
+#
+# This script is to be run from proxmark root folder inside the docker env
+# docker/fedora-43/run_tests.sh;
+
+sudo yum -y update
+tools/release_tests.sh
