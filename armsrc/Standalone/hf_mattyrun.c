24d6013f10638bbc79da1fc62929af683b63b6c5
iceman1001 <iceman@iuse.se>
2025-06-11 12:26:11 +0200

    making the spiffs write guarded if firmware was compiled with spiffs flash enabled

--- armsrc/Standalone/hf_mattyrun.c
+++ armsrc/Standalone/hf_mattyrun.c
@@ -535,12 +535,14 @@ void RunMod(void) {
                 SpinErr(LED_D, 50, 8);
                 partialEmulation = true;
             } else {
+#if WITH_FLASH                
                 DbpString("[" _GREEN_("+") "] " _GREEN_("Emulator memory filled completely. Start storing card in spiff memory."));
                 uint8_t *emCARD = BigBuf_get_EM_addr();
                 char dumpFileName[30] = {0};
                 sprintf(dumpFileName, DUMP_FILE, mattyrun_card.uid[0], mattyrun_card.uid[1], mattyrun_card.uid[2], mattyrun_card.uid[3]);
                 rdv40_spiffs_write(dumpFileName, emCARD, 1024, RDV40_SPIFFS_SAFETY_SAFE);
                 Dbprintf("[" _GREEN_("+") "] " _GREEN_("Stored card on %s"), dumpFileName);
+#endif                 
             }
 
             state = STATE_EMULATE;
