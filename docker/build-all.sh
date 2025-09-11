b0271c4b684f8f59fcca55fc21100371178a4133
Philippe Teuwen <phil@teuwen.org>
2025-09-11 12:45:41 +0200

    update docker setups

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for os in archlinux debian-12-bookworm debian-12-bookworm-arm64 debian-12-bookworm-armhf debian-13-trixie fedora-36 fedora-37 homebrew kali opensuse-leap opensuse-tumbleweed parrot-core-latest ubuntu-20.04 ubuntu-22.04; do
+for os in archlinux debian-12-bookworm debian-12-bookworm-arm64 debian-12-bookworm-armhf debian-13-trixie debian-14-forky fedora-41 fedora-42 fedora-43 homebrew kali opensuse-leap opensuse-tumbleweed parrot-core-latest ubuntu-24.04 ubuntu-24.10 ubuntu-25.04; do
+  echo -e "\n\n================= Building for $os ======================\n"
   (  cd $os && ../build.sh )
 done
