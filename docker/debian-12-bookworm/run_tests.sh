a65920709036fe22245a2a6e6146a9553e1ef420
Philippe Teuwen <phil@teuwen.org>
2026-02-21 16:07:20 +0100

    remove debian oldstable docker files

--- docker/debian-12-bookworm/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This script is to be run from proxmark root folder inside the docker env
-# docker/debian-12-bookworm/run_tests.sh;
-
-sudo apt update && sudo apt upgrade -y
-tools/release_tests.sh
