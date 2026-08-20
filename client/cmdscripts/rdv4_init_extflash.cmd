72d72ddc9d8ae6b78050496f4aa500835f047894
Philippe Teuwen <phil@teuwen.org>
2026-08-20 10:11:42 +0200

    update extflash init scripts

--- client/cmdscripts/rdv4_init_extflash.cmd
+++ client/cmdscripts/rdv4_init_extflash.cmd
@@ -4,4 +4,5 @@ mem load -f mfc_default_keys --mfc
 mem load -f t55xx_default_pwds --t55xx
 mem load -f iclass_default_keys --iclass
 mem load -f mfulc_default_keys --ulc
+mem load -f mfulaes_default_keys --aes
 lf t55xx deviceconfig -z -p
