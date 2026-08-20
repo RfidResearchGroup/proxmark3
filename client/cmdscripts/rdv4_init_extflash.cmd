468227148862e7630e33b9aadac342496856b9eb
Philippe Teuwen <phil@teuwen.org>
2026-08-20 17:49:12 +0200

    add sleep to rdv4_init_extflash t55xx config

--- client/cmdscripts/rdv4_init_extflash.cmd
+++ client/cmdscripts/rdv4_init_extflash.cmd
@@ -6,6 +6,9 @@ mem load -f iclass_default_keys --iclass
 mem load -f mfulc_default_keys --ulc
 mem load -f mfulaes_default_keys --aes
 lf t55xx deviceconfig --r0 -a 29 -b 17 -c 15 -d 47 -e 15 -p
+msleep --ms 200
 lf t55xx deviceconfig --r1 -a 29 -b 17 -c 18 -d 50 -e 15 -p
+msleep --ms 200
 lf t55xx deviceconfig --r2 -a 29 -b 17 -c 18 -d 40 -e 15 -p
+msleep --ms 200
 lf t55xx deviceconfig --r3 -a 29 -b 17 -c 15 -d 31 -e 15 -f 47 -g 63 -p
