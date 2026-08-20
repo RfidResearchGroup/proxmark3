08f8dc8692eaf143867af9351374ca4176ee290e
Philippe Teuwen <phil@teuwen.org>
2026-08-20 14:28:08 +0200

    CARDHOPPER: document how to set CARDHOPPER_USB

--- armsrc/Standalone/hf_cardhopper.c
+++ armsrc/Standalone/hf_cardhopper.c
@@ -32,6 +32,9 @@
 #include "usb_cdc_apis.h"
 #include "usb_read_ng.h"
 
+// To enable CARDHOPPER_USB, add the following line in your Makefile.platform
+// STANDALONE_PLATFORM_DEFS+=-DCARDHOPPER_USB
+
 #ifdef CARDHOPPER_USB
 #define cardhopper_write usb_write
 #define cardhopper_read usb_read_ng
