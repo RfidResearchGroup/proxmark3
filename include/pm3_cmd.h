75f723647d82faf388b9d2701079302d6721b8e2
Niel Nielsen <nieldk@gmail.com>
2026-08-25 08:02:27 +0200

    Define CMD_PM5_BWM_AUTOOFF for USB unplug
    Added command definition for PM5 automatic power-off feature.
    
    Signed-off-by: Niel Nielsen <nieldk@gmail.com>

--- include/pm3_cmd.h
+++ include/pm3_cmd.h
@@ -644,6 +644,8 @@ typedef struct {
 #define CMD_PM5_BWM_SET_CAP 0x0179
 // PM5, enable/disable BWM battery charging (AW32001E CEB). Used by `hw bwmcharge`.
 #define CMD_PM5_BWM_CHARGE_EN 0x017A
+// PM5, toggle automatic power-off on USB unplug. Used by `hw bwmautooff`.
+#define CMD_PM5_BWM_AUTOOFF 0x017B
 // For low-frequency tags
 #define CMD_LF_TI_READ 0x0202
 #define CMD_LF_TI_WRITE 0x0203
