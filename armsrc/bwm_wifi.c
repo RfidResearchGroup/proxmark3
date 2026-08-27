b60f5e2ac8e5037dcb4c411beb08326d3a973c8d
Niel Nielsen <nieldk@gmail.com>
2026-08-27 13:30:55 +0200

    Add PM3 command includes and fix function closing
    Signed-off-by: Niel Nielsen <nieldk@gmail.com>

--- armsrc/bwm_wifi.c
+++ armsrc/bwm_wifi.c
@@ -4,6 +4,7 @@
 #include "bwm_wifi.h"
 #include "bwm_uart_at32.h"   // bwm_uart_write / bwm_uart_rx_available / bwm_uart_read
 #include "bwm_forward.h"     // app_com header bytes + BWM_CMD_* forward codes
+#include "pm3_cmd.h"         // PM3_SUCCESS / PM3_ETIMEOUT / PM3_EFAILED / PM3_EOVFLOW
 #include "ticks_apis.h"      // GetTickCount / GetTickCountDelta
 #include "dbprint.h"
 #include "string.h"
