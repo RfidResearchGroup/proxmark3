e5ed0d52e19fd16bc2845f34d0d28d0d1577ef0e
Niel Nielsen <nieldk@gmail.com>
2026-09-03 09:16:21 +0200

    Add BWM OTA chunk size definition
    Signed-off-by: Niel Nielsen <nieldk@gmail.com>

--- include/pm3_cmd.h
+++ include/pm3_cmd.h
@@ -887,6 +887,11 @@ typedef struct {
 #define BWM_OTA_ACTION_BEGIN 0x00
 #define BWM_OTA_ACTION_WRITE 0x01
 #define BWM_OTA_ACTION_END   0x02
+// Max firmware bytes per WRITE action. Bounded by the BWM app_com UART link's
+// internal frame buffer (bwm_wifi.c: bwm_cmd()'s `frame[8 + 256]`), which is
+// far smaller than the USB link's max_cmd_data_size - do not derive this from
+// g_conn.max_cmd_data_size.
+#define BWM_OTA_CHUNK_MAX     256
 // CMD_PM5_BWM_WIFI payload: [action:u8][port:u16 LE][ssid\0][pwd\0][hostname\0]
 #define BWM_WIFI_ACTION_START  0x00   // join AP + start TCP server
 #define BWM_WIFI_ACTION_STOP   0x01   // tear down, back to BLE-only
