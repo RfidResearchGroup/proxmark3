c6339ad99c0e8f07bb3863e06a4f0f21aa45e917
Niel Nielsen <nieldk@gmail.com>
2026-09-12 10:35:40 +0200

    Change RgbLedSet return type to bool
    Signed-off-by: Niel Nielsen <nieldk@gmail.com>

--- common_arm/rgb/rgb_apis.h
+++ common_arm/rgb/rgb_apis.h
@@ -10,6 +10,6 @@
 // driven by an I2C RGB controller). Other platforms don't build this module, so
 // callers must guard usage accordingly (e.g. the CMD_PM5_RGB_SET handler).
 //-----------------------------------------------------------------------------
-void RgbLedSet(uint8_t r, uint8_t g, uint8_t b);
+bool RgbLedSet(uint8_t r, uint8_t g, uint8_t b);
 
 #endif // RGB_APIS_H_
