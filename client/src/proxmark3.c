dc0110c29d320e04b37ea436ee2924901407ae72
Philippe Teuwen <phil@teuwen.org>
2026-09-18 00:22:18 +0200

    Release v4.23346 - Frosty Lemon

--- client/src/proxmark3.c
+++ client/src/proxmark3.c
@@ -47,8 +47,8 @@ static int mainret = PM3_SUCCESS;
 
 #ifndef LIBPM3
 #define BANNERMSG1 ""
-#define BANNERMSG2 ""
-#define BANNERMSG3 ""
+#define BANNERMSG2 
+#define BANNERMSG3 "    Release... v4.23346 - Frosty Lemon"
 
 typedef enum LogoMode { UTF8, ANSI, ASCII, UTF8_PM5, ANSI_PM5, ASCII_PM5 } LogoMode;
 
